6939b74d9f450af2bd2f511b78cdbc47d7e4b933
David Riera <david.riera@somenergia.coop>
2020-04-21 17:33:26 +0200

    passing parameters to the app from outside

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-//import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = document.getElementById('root')
+// const props = [].filter.call(root.attributes, attr => /^data-/.test(attr.name))
+const props = {}
+const attrs = Object.keys(root.dataset)
+attrs.forEach(
+  (name, index) => { props[name] = root.dataset[name] }
+)
+
+ReactDOM.render(<App {...props} />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+// serviceWorker.unregister()
