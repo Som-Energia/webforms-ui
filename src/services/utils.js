48ae8ede3da7aebf30cd9997e86a14cc699bb309
David Riera <david.riera@somenergia.coop>
2020-05-05 16:16:12 +0200

    function normalize data form

---
+++ src/services/utils.js
@@ -0,0 +1,21 @@
+const THOUSANDS_CONVERSION_FACTOR = 1000
+
+export const normalizeFormData = (params) => {
+  const { modify, contact, token } = params
+
+  const data = {
+    tarifa: modify?.tariff,
+    tension: modify?.phases,
+    attachments: [...modify?.attachments, ...modify?.power_attachments],
+    potencia: modify?.changePower ? Math.round(modify?.power * THOUSANDS_CONVERSION_FACTOR) : undefined,
+    potencia_p2: modify?.changePower && modify?.moreThan15Kw ? Math.round(modify?.power2 * THOUSANDS_CONVERSION_FACTOR) : undefined,
+    potencia_p3: modify?.changePower && modify?.moreThan15Kw ? Math.round(modify?.power3 * THOUSANDS_CONVERSION_FACTOR) : undefined,
+    discriminacio: modify?.fare,
+    contact_name: contact?.contactName,
+    contact_surname: contact?.contactSurname,
+    contact_phone: contact?.phone,
+    token: token
+  }
+
+  return data
+}
