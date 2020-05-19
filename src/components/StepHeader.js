f069f60dfccb26d483630b92813877ffc268eeb3
David Riera <david.riera@somenergia.coop>
2020-05-19 17:03:17 +0200

    added step header component

---
+++ src/components/StepHeader.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const StepHeader = ({title}) => {
+  return (
+    <div className="header">
+      <div className="header__container">
+        <div className="header__text">
+          {title}
+        </div>
+      </div>
+      <div className="header__after"></div>
+    </div>
+  )
+}
+
+export default StepHeader
