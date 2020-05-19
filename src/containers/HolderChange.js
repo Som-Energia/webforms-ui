643042b4a0498a8a0aaf47db7ffd1dfda317a38f
David Riera <david.riera@somenergia.coop>
2020-05-19 18:22:36 +0200

    added formik and yup to holder change

--- src/containers/HolderChange.js
+++ src/containers/HolderChange.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
+import { Formik, Form } from 'formik'
+import * as Yup from 'yup'
 
 import { makeStyles } from '@material-ui/core/styles'
-
 import Button from '@material-ui/core/Button'
 import Box from '@material-ui/core/Box'
 import Container from '@material-ui/core/Container'
 import Paper from '@material-ui/core/Paper'
-
 import Slide from '@material-ui/core/Slide'
 
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos'
@@ -18,6 +18,11 @@ import CUPS from './HolderChange/CUPS'
 import PersonalData from './HolderChange/PersonalData'
 import BecomeMember from './HolderChange/BecomeMember'
 import VoluntaryCent from './HolderChange/VoluntaryCent'
+import SpecialCases from './HolderChange/SpecialCases'
+import IBAN from './HolderChange/IBAN'
+import Review from './HolderChange/Review'
+
+import DisplayFormikState from '../components/DisplayFormikState'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -31,12 +36,6 @@ const useStyles = makeStyles((theme) => ({
   actionsContainer: {
     display: 'flex',
     justifyContent: 'space-between'
-  },
-  withoutLabel: {
-    marginTop: theme.spacing(3)
-  },
-  textField: {
-    width: '25ch'
   }
 }))
 
@@ -53,7 +52,32 @@ function HolderChange (props) {
     <CUPS validate={setValidated} />,
     <PersonalData validate={setValidated} />,
     <BecomeMember validate={setValidated} />,
-    <VoluntaryCent validate={setValidated} />
+    <VoluntaryCent validate={setValidated} />,
+    <SpecialCases validate={setValidated} />,
+    <IBAN validate={setValidated} />,
+    <Review validate={setValidated} />
+  ]
+
+  const getWizardSteps = (props) => {
+    return [
+      <VAT validate={setValidated} {...props} />,
+      <CUPS validate={setValidated} {...props} />,
+      <PersonalData validate={setValidated} {...props} />,
+      <BecomeMember validate={setValidated} {...props} />,
+      <VoluntaryCent validate={setValidated} {...props} />,
+      <SpecialCases validate={setValidated} {...props} />,
+      <IBAN validate={setValidated} {...props} />,
+      <Review validate={setValidated} {...props} />
+    ]
+  }
+
+  const validationSchemas = [
+    Yup.object().shape({
+      nif: Yup.string().required(t('FILL_NIF'))
+    }),
+    Yup.object().shape({
+      field2: Yup.string().required('Field1 Is Required')
+    })
   ]
 
   useEffect(() => {
@@ -61,60 +85,80 @@ function HolderChange (props) {
     i18n.changeLanguage(language)
   }, [props.match.params.language, i18n])
 
-  const getCurrentStep = () => {
-    return wizardSteps[activeStep]
-  }
-
-  const nextStep = () => {
+  const nextStep = props => {
+    console.log('Next step')
     const next = activeStep + 1
-    return (next < wizardSteps.length) ? setActiveStep(next) : null
+    const last = wizardSteps.length - 1
+    props.submitForm().then(() => {
+      if (props.isValid) {
+        props.validateForm()
+        props.setTouched({})
+        setActiveStep(Math.min(next, last))
+      }
+    })
   }
 
-  const backStep = () => {
-    const back = activeStep - 1
-    return (back >= 0) ? setActiveStep(back) : null
+  const prevStep = () => {
+    console.log('Prev step')
+    const prev = activeStep - 1
+    setActiveStep(Math.max(0, prev))
+  }
+
+  const handleSubmit = props => {
+    console.log(props)
   }
 
   return (
     <Container maxWidth="md" className={classes.root}>
-      {
-        wizardSteps.map((step, index) => (
-          <Slide key={index} direction="right" in={showAll || index === activeStep}>
-            <Paper elevation={3} className={classes.stepContainer}>
-              <Box mx={4} my={3}>
-                {step}
-              </Box>
-              <Box mx={4} my={3}>
-                <div className={classes.actionsContainer}>
-                  {
-                    <Button
-                      className={classes.button}
-                      startIcon={<ArrowBackIosIcon />}
-                      disabled={(activeStep === 0)}
-                      onClick={backStep}
-                    >
-                      {t('PAS_ANTERIOR')}
-                    </Button>
-                  }
-                  {
-                    <Button
-                      type="submit"
-                      className={classes.button}
-                      variant="contained"
-                      color="primary"
-                      endIcon={<ArrowForwardIosIcon />}
-                      disabled={!isValidated}
-                      onClick={nextStep}
-                    >
-                      {t('SEGUENT_PAS')}
-                    </Button>
-                  }
-                </div>
-              </Box>
-            </Paper>
-          </Slide>
-        ))
-      }
+      <Formik
+        onSubmit={handleSubmit}
+        enableReinitialize
+        initialValues={{ nif: '', cups: '' }}
+        validationSchema={validationSchemas[activeStep]}
+        render={props => (
+          <Form>
+            {
+              getWizardSteps(props).map((step, index) => (
+                <Slide key={index} direction="right" in={showAll || index === activeStep}>
+                  <Paper elevation={3} className={classes.stepContainer}>
+                    <Box mx={4} my={3}>
+                      {step}
+                    </Box>
+                    <Box mx={4} my={3}>
+                      <div className={classes.actionsContainer}>
+                        {
+                          <Button
+                            className={classes.button}
+                            startIcon={<ArrowBackIosIcon />}
+                            disabled={(activeStep === 0)}
+                            onClick={prevStep}
+                          >
+                            {t('PAS_ANTERIOR')}
+                          </Button>
+                        }
+                        {
+                          <Button
+                            type="button"
+                            className={classes.button}
+                            variant="contained"
+                            color="primary"
+                            endIcon={<ArrowForwardIosIcon />}
+                            disabled={!props.isValid}
+                            onClick={ () => nextStep(props)}
+                          >
+                            {t('SEGUENT_PAS')}
+                          </Button>
+                        }
+                      </div>
+                    </Box>
+                  </Paper>
+                </Slide>
+              ))
+            }
+            <DisplayFormikState {...props} />
+          </Form>
+        )}
+      />
     </Container>
   )
 }
