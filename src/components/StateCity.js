78b74fbce3eb7b5a1631c56e4850598cafe79b03
David Riera <david.riera@somenergia.coop>
2020-06-08 10:06:57 +0200

    fixed state and city selector and improved validations on personal data step

--- src/components/StateCity.js
+++ src/components/StateCity.js
@@ -1,43 +1,81 @@
 import React, { useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 
+import CircularProgress from '@material-ui/core/CircularProgress'
 import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField'
 import MenuItem from '@material-ui/core/MenuItem'
+import InputAdornment from '@material-ui/core/InputAdornment'
 
 import { getProvincies, getMunicipis } from '../services/api'
 
 const StateCity = (props) => {
   const { t } = useTranslation()
-  const { stateName, stateId, cityName, cityId } = props
+  const { stateName, stateId, stateInitial, cityName, cityId, cityInitial, stateError, stateHelperText, cityError, cityHelperText, onChange } = props
 
-  const [state, setState] = useState()
-
+  const [state, setState] = useState(stateInitial)
   const [states, setStates] = useState([])
-  const [cities, setcities] = useState([])
+  const [isLoadingStates, setIsLoadingStates] = useState(false)
+
+  const [city, setCity] = useState(cityInitial)
+  const [cities, setCities] = useState([])
+  const [isLoadingCities, setIsLoadingCities] = useState(false)
 
   useEffect(() => {
+    setIsLoadingStates(true)
     getProvincies()
       .then(response => {
-        console.log(response?.data?.provincies)
         setStates(response?.data?.provincies)
+        setIsLoadingStates(false)
+      }).catch(error => {
+        console.log(error)
+        setIsLoadingStates(false)
       })
   }, [])
 
   useEffect(() => {
-    getMunicipis(state)
-      .then(response => {
-        console.log(response?.data?.municipis)
-      })
+    if (state) {
+      setIsLoadingCities(true)
+      getMunicipis(state)
+        .then(response => {
+          setCities(response?.data?.municipis)
+          setIsLoadingCities(false)
+        }).catch(error => {
+          console.log(error)
+          setIsLoadingCities(false)
+        })
+    }
   }, [state])
 
+  const getStateName = (value) => {
+    const state = states.find(state => value === state.id)
+    return state?.name
+  }
+
+  const getCityName = (value) => {
+    const city = cities.find(city => value === city.id)
+    return city?.name
+  }
+
   const handleStateChange = (event) => {
-    console.log(event.target.value)
     setState(event.target.value)
+    setCity('')
+    onChange({
+      state: event.target.value,
+      city: '',
+      stateName: '',
+      cityName: ''
+    })
   }
 
   const handleCityChange = (event) => {
-    console.log(event.target.value)
+    setCity(event.target.value)
+    onChange({
+      state: state,
+      city: event.target.value,
+      stateName: getStateName(state),
+      cityName: getCityName(event.target.value)
+    })
   }
 
   return (
@@ -53,6 +91,17 @@ const StateCity = (props) => {
           required
           fullWidth
           disabled={!states.length}
+          value={state}
+          error={stateError}
+          helperText={stateHelperText}
+          InputProps={{
+            endAdornment:
+              <InputAdornment position="end">
+                { isLoadingStates &&
+                  <CircularProgress size={24} />
+                }
+              </InputAdornment>
+          }}
         >
           <MenuItem key="0" value="">{t('STATE')}</MenuItem>
           {
@@ -71,6 +120,17 @@ const StateCity = (props) => {
           required
           fullWidth
           disabled={!cities.length}
+          value={city}
+          error={cityError}
+          helperText={cityHelperText}
+          InputProps={{
+            endAdornment:
+              <InputAdornment position="end">
+                { isLoadingCities &&
+                  <CircularProgress size={24} />
+                }
+              </InputAdornment>
+          }}
         >
           <MenuItem key="0" value="">{t('CITY')}</MenuItem>
           {
