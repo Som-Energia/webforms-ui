2732ee3e0a7f5660f06200c0db1c03cfed2e366b
David Riera <david.riera@somenergia.coop>
2020-06-08 09:51:09 +0200

    fixed modify contract switches position and upload performance

--- src/containers/ModifyContract/Intro.js
+++ src/containers/ModifyContract/Intro.js
@@ -5,7 +5,6 @@ import { makeStyles } from '@material-ui/core/styles'
 
 import Box from '@material-ui/core/Box'
 import Button from '@material-ui/core/Button'
-import Divider from '@material-ui/core/Divider'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 
