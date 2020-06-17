59173c9ef65d950264470dee7427c6135a26b9a5
David Riera <david.riera@somenergia.coop>
2020-06-17 12:19:34 +0200

    holder change normalize contract and post service

--- src/services/utils.js
+++ src/services/utils.js
@@ -34,3 +34,50 @@ export const checkPhisicalVAT = (vat) => {
   var firstchar = vat[0]
   return '0123456789KLMXYZ'.indexOf(firstchar) !== -1
 }
+
+export const normalizeHolderChange = (contract) => {
+  const normalContract = Object.assign({}, contract)
+
+  if (normalContract?.supply_point?.verified) {
+    delete normalContract.supply_point.verified
+  }
+  if (normalContract?.supply_point?.status) {
+    delete normalContract.supply_point.status
+  }
+  if (normalContract?.holder?.language?.code) {
+    normalContract.holder.language = normalContract.holder.language.code
+  }
+  if (normalContract?.holder?.state?.id) {
+    normalContract.holder.state = normalContract.holder.state.id
+  }
+  if (normalContract?.holder?.city?.id) {
+    normalContract.holder.city = normalContract.holder.city.id
+  }
+  if (normalContract?.holder?.vatexists) {
+    delete normalContract.holder.vatexists
+  }
+  if (normalContract?.holder?.vatvalid) {
+    delete normalContract.holder.vatvalid
+  }
+  if (normalContract?.holder?.phone2 === '') {
+    delete normalContract.holder.phone2
+  }
+  if (normalContract?.holder?.proxynif_valid) {
+    delete normalContract.holder.proxynif_valid
+  }
+  if (normalContract?.member.become_member === undefined) {
+    normalContract.member.is_member = true
+    normalContract.member.become_member = false
+  }
+  if (normalContract.payment.iban) {
+    normalContract.payment.iban = normalContract.payment.iban.split(' ').join('')
+  }
+  return normalContract
+}
+
+export const specialCaseType = (specialCases) => {
+  if (specialCases.reason_death) return 'SPECIAL_CASES_DEATH'
+  else if (specialCases.reason_merge) return 'SPECIAL_CASES_MERGE'
+  else if (specialCases.reason_electrodep) return 'SPECIAL_CASES_ELECTRODEP'
+  else return ''
+}
