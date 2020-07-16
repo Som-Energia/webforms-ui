5dd752d8ab418bba785578d9e4a8df6863f1e8c9
David Riera <david.riera@somenergia.coop>
2020-07-16 17:25:16 +0200

    changed loading svg animation

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,16 +4,20 @@ import { makeStyles } from '@material-ui/core/styles'
 import loading from '../images/loading.svg'
 
 const useStyles = makeStyles((theme) => ({
-  loading: {}
+  loading: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 }))
 
 const Loading = () => {
   const classes = useStyles()
 
   return (
-    <>
-      <img className={classes.loading} alt="Loading..." src={loading} />
-    </>
+    <div className={classes.loading}>
+      <img alt="Loading..." src={loading} />
+    </div>
   )
 }
 
