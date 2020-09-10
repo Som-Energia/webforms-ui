e10fa7ed9fdf922bee824cfabd82a94871e09477
Benjami <benjami94@gmail.com>
2020-09-10 09:57:57 +0200

    Add D1 detail screen and D1Validation container

---
+++ src/containers/D1Detail.js
@@ -0,0 +1,202 @@
+import React, { useState, useEffect, useCallback } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import { makeStyles } from '@material-ui/core/styles'
+
+import Alert from '@material-ui/lab/Alert'
+import AlertTitle from '@material-ui/lab/AlertTitle'
+
+import Grow from '@material-ui/core/Grow'
+import Stepper from '@material-ui/core/Stepper'
+import Step from '@material-ui/core/Step'
+import StepLabel from '@material-ui/core/StepLabel'
+import StepContent from '@material-ui/core/StepContent'
+
+import D1Validation from './CaseDetail/D1Validation'
+import Intro from './CaseDetail/Intro'
+import Params from './ModifyContract/Params'
+import Contact from './ModifyContract/Contact'
+import Resume from './ModifyContract/Resume'
+import RefuseD1 from './CaseDetail/RefuseD1'
+import AcceptD1 from './CaseDetail/AcceptD1'
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    backgroundColor: '#f2f2f2'
+  },
+  actionsContainer: {
+    marginBottom: theme.spacing(2)
+  },
+  resetContainer: {
+    padding: theme.spacing(2)
+  },
+  responseContainer: {
+    padding: theme.spacing(1)
+  },
+  stepper: {
+    backgroundColor: '#f2f2f2',
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+    paddingBottom: theme.spacing(1)
+  },
+  stepLabel: {
+    fontSize: '1.15rem'
+  }
+}))
+
+const steps = [
+  'DETAIL_D1_TITLE',
+  'ACCEPT_OR_REFUSE_TITLE',
+  'MODIFY_POTTAR_SELECT_TITLE',
+  'MODIFY_POTTAR_CONTACT_TITLE',
+  'REVISIO_CONFIRMACIO_DADES'
+]
+
+function D1Detail (props) {
+  const classes = useStyles()
+  const { t, i18n } = useTranslation()
+
+  useEffect(() => {
+    const language = props.match.params.language
+    i18n.changeLanguage(language)
+  }, [props.match.params.language, i18n])
+
+  const [activeStep, setActiveStep] = useState(0)
+  const [data, setData] = useState(props?.templateProps)
+  const { setFieldValue } = props
+
+  const handleStepChanges = useCallback((params) => {
+    setData({ ...data, ...params })
+  }, [data])
+
+  const handlePost = async (values) => {
+    data.sending = true
+    //setFieldValue('data.sending', true)
+    console.log("SENDING TO API")
+    data.sending = false
+    //setFieldValue('data.sending', false)
+  }
+
+  const getStepContent = (step) => {
+    switch (step) {
+      case 0:
+        return <D1Validation
+          handleRefuseClick={handleRefuseClick}
+          handleAcceptClick={handleAcceptClick}
+          handleStepChanges={handleStepChanges}
+          params={data}
+        />
+      case 1:
+        if (data?.validate) {
+          return <AcceptD1
+            prevStep={() => prevStep()}
+            nextStep={() => nextStep(2)}
+            handlePost={() => handlePost()}
+            handleStepChanges={handleStepChanges}
+          />
+        }
+        else {
+          return <RefuseD1
+            handlePost={() => handlePost()}
+            prevStep={() => prevStep()}
+            handleStepChanges={handleStepChanges}
+          />
+        }
+      case 3:
+        return <Intro
+          prevStep={() => prevStep()}
+          nextStep={() => nextStep(2)}
+          handleStepChanges={handleStepChanges}
+        />
+      case 4:
+        return <Params
+          nextStep={() => nextStep(3)}
+          prevStep={() => prevStep()}
+          handleStepChanges={handleStepChanges}
+          params={data?.modify}
+        />
+      case 5:
+        return <Contact
+          nextStep={() => nextStep(4)}
+          prevStep={() => prevStep()}
+          handleStepChanges={handleStepChanges}
+          params={data?.contact}
+        />
+      default:
+        return <Resume
+          nextStep={() => nextStep(5)}
+          prevStep={prevStep}
+          handleStepChanges={handleStepChanges}
+          params={data}
+        />
+    }
+  }
+
+  const handleRefuseClick = () => {
+    data.validate = false
+    nextStep(1)
+  }
+
+  const handleAcceptClick = () => {
+    data.validate = true
+    nextStep(1)
+  }
+
+  const nextStep = (step) => {
+    setActiveStep(step)
+  }
+
+  const prevStep = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1)
+  }
+
+  return (
+    <div className={classes.root}>
+      { (data?.to_validate &&
+        <>
+          <Stepper className={classes.stepper} activeStep={activeStep} orientation="vertical">
+            {steps.map((label, index) => (
+              <Step key={label}>
+                <StepLabel error={ (index === steps.length - 1) && (data?.error !== undefined) } ><span className={classes.stepLabel}>{t(label)}</span></StepLabel>
+                <StepContent>
+                  {getStepContent(index)}
+                </StepContent>
+              </Step>
+            ))}
+          </Stepper>
+          { data?.error &&
+            <Grow in={data?.error !== undefined}>
+              <div className={classes.responseContainer}>
+                <Alert severity="error">
+                  <AlertTitle>{t('ERROR_POST_MODIFY')}</AlertTitle>
+                  {t(data?.error?.code)}
+                </Alert>
+              </div>
+            </Grow>
+          }
+          { data?.response &&
+            <Grow in={data?.response !== undefined}>
+              <div className={classes.responseContainer}>
+                <Alert severity="success">
+                  <AlertTitle>{t('MODIFY_POTTAR_SUCCESS_TITTLE')}</AlertTitle>
+                  {t('MODIFY_POTTAR_SUCCESS_MESSAGE')}
+                </Alert>
+              </div>
+            </Grow>
+          }
+        </>
+      ) ||
+        <D1Validation
+          nextStep={() => nextStep(1)}
+          handleStepChanges={handleStepChanges}
+          params={data}
+        />
+      }
+
+    </div>
+  )
+}
+
+export default D1Detail
