2b10f6d28095a92f26e19d5e829d118908ac8f86
David Riera <david.riera@somenergia.coop>
2020-09-14 18:14:16 +0200

    added new member form

---
+++ src/containers/Member/VAT.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+import Box from '@material-ui/core/Box'
+import FormHelperText from '@material-ui/core/FormHelperText'
+import Typography from '@material-ui/core/Typography'
+
+import StepHeader from '../../components/StepHeader'
+import VATField from '../../components/VATField'
+
+function VAT (props) {
+  const { values, setFieldValue, setFieldTouched, handleBlur, touched, errors } = props
+  const { t } = useTranslation()
+
+  const onChangeVAT = ({ vat, isPhisical, valid }) => {
+    setFieldValue('member.vat', vat)
+    setFieldValue('member.isphisical', isPhisical)
+    setFieldValue('member.vatvalid', valid)
+    setFieldTouched('member.vat', true)
+  }
+
+  return (
+    <>
+      <StepHeader title={t('NEW_MEMBER')} />
+      <Typography variant="body1"
+        dangerouslySetInnerHTML={{ __html: t('NEW_MEMBER_DESC') }}
+      />
+      <Box mt={3} mb={1}>
+        <VATField
+          id="vat"
+          name="member.vat"
+          label={t('VAT_LABEL')}
+          variant="outlined"
+          fullWidth
+          required
+          autoFocus={true}
+          value={values?.member?.vat}
+          onChange={onChangeVAT}
+          onBlur={handleBlur}
+          error={(errors?.member?.vat && touched?.member?.vat) ||
+            (touched?.member?.vat && values?.member?.vatvalid === false)
+          }
+          helperText={(touched?.member?.vat && errors?.member?.vat) ||
+            (touched?.member?.vat && errors?.member?.vatvalid)
+          }
+        />
+      </Box>
+      <Box mt={4} mb={3}>
+        <FormHelperText dangerouslySetInnerHTML={{ __html: t('NEW_MEMBER_NO_VAT_HELP') }}></FormHelperText>
+      </Box>
+    </>
+  )
+}
+
+export default VAT
