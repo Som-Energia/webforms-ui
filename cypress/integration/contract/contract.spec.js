084988c4bb4160f045be912e6426d6aedaa75a71
Marta <marta.fernandez@somenergia.coop>
2020-10-21 15:14:47 +0200

    add cypress tests for legal person

--- cypress/integration/contract/contract.spec.js
+++ cypress/integration/contract/contract.spec.js
@@ -463,4 +463,151 @@ describe('Contract', () => {
     cy.get('[data-cy=next]').click()
   })
 
+  describe('Juridic Person', function () {
+    beforeEach(function () {
+      cy.get('#memberNumber')
+        .clear()
+        .type(this.data.juridic_member.number)
+        .should('have.value', this.data.juridic_member.number)
+
+      cy.get('#vat')
+        .clear()
+        .type(this.data.juridic_member.vat)
+        .should('have.value', this.data.juridic_member.vat)
+
+      cy.get('[data-cy=next]').click()
+
+      cy.get('#cups')
+        .clear()
+        .type(this.data.supplyPoint.cups)
+        .should('have.value', this.data.supplyPoint.cups)
+
+      cy.wait(500)
+      cy.get(`[data-value="${this.data.supplyPoint.hasService}"]`).click()
+
+      cy.get('[data-cy=next]').click()
+
+      cy.get('#supply_point_address')
+        .clear()
+        .type(this.data.supplyPoint.address)
+        .should('have.value', this.data.supplyPoint.address)
+
+      cy.get('#supply_point_number')
+        .clear()
+        .type(this.data.supplyPoint.number)
+        .should('have.value', this.data.supplyPoint.number)
+
+      cy.get('#supply_point_postal_code')
+        .clear()
+        .type(this.data.supplyPoint.postalCode)
+        .should('have.value', this.data.supplyPoint.postalCode)
+
+      cy.get('#supply_point_state').click()
+      cy.get(`[data-value="${this.data.supplyPoint.state}"]`).click()
+
+      cy.get('#supply_point_city').click()
+      cy.get(`[data-value="${this.data.supplyPoint.city}"]`).click()
+
+      cy.get('#supply_point_is_housing').click()
+      cy.get(`[data-value="${this.data.supplyPoint.isHousing}"]`).click()
+
+      cy.get('#supply_point_cnae')
+        .should('have.value', this.data.supplyPoint.cnae)
+
+      cy.get('[name="supply_point_accepted"]').click()
+      cy.get('[data-cy=accept]').click()
+
+      cy.get('[data-cy=next]').click()
+
+      cy.get('#rate').click()
+      cy.get(`[data-value="${this.data.fare20A}"]`).click()
+
+      cy.get('#power')
+        .type(this.data.power).should('have.value', this.data.power)
+
+      cy.get('[data-cy=next]').click()
+
+    })
+
+    afterEach(function () {
+      cy.get(`[data-value="${this.data.holder.voluntaryCent}"]`).click()
+      cy.get('[data-cy=next]').click()
+      cy.get('[name="payment.iban"]')
+        .clear()
+        .type(this.data.holder.iban).should('have.value', this.data.holder.iban)
+
+      cy.get('[name="payment.sepa_accepted"]').click()
+      cy.get('[data-cy=accept]').click()
+      cy.get('[data-cy=next]').click()
+
+      cy.contains('€/kWh')
+    })
+
+    it('Same juridic person', function () {
+      cy.get('[name="holder.vat"]')
+        .type(this.data.juridic_member.vat).should('have.value', this.data.juridic_member.vat)
+      cy.wait(800)
+      cy.get(`[data-value="${this.data.juridic_holder.previousHolder}"]`).click()
+
+      cy.get('[name="legal_person_accepted"]').click()
+      cy.get('[data-cy=accept]').click()
+
+      cy.get('[data-cy=next]').click()
+
+    })
+
+    it('Different juridic person', function () {
+      cy.get('[name="holder.vat"]')
+        .type(this.data.juridic_holder.vat).should('have.value', this.data.juridic_holder.vat)
+      cy.wait(800)
+      cy.get(`[data-value="${this.data.juridic_holder.previousHolder}"]`).click()
+
+      cy.get('[name="legal_person_accepted"]').click()
+      cy.get('[data-cy=accept]').click()
+
+      cy.get('[data-cy=next]').click()
+
+      cy.get('[name="holder.name"]')
+        .type(this.data.juridic_holder.name).should('have.value', this.data.juridic_holder.name)
+
+      cy.get('[name="holder.proxyname"]')
+        .type(this.data.juridic_holder.proxyname).should('have.value', this.data.juridic_holder.proxyname)
+
+      cy.get('[name="holder.proxynif"]')
+        .type(this.data.juridic_holder.proxynif).should('have.value', this.data.juridic_holder.proxynif)
+
+      cy.get('[name="holder.address"]')
+        .type(this.data.holder.address).should('have.value', this.data.holder.address)
+
+      cy.get('[name="holder.postal_code"]')
+        .type(this.data.holder.postalCode).should('have.value', this.data.holder.postalCode)
+
+      cy.get('#holder_state').click()
+      cy.get(`[data-value="${this.data.holder.stateCode}"]`).click()
+
+      cy.get('#holder_city').click()
+      cy.get(`[data-value="${this.data.holder.cityCode}"]`).click()
+
+      cy.get('[name="holder.email"]')
+        .clear()
+        .type(this.data.holder.email).should('have.value', this.data.holder.email)
+
+      cy.get('[name="holder.email2"]')
+        .clear()
+        .type(this.data.holder.email).should('have.value', this.data.holder.email)
+
+      cy.get('[name="holder.phone1"]')
+        .type(this.data.holder.phone).should('have.value', this.data.holder.phone)
+
+      cy.get('#holder_lang').click()
+      cy.get('[data-value="ca_ES"]').click()
+
+      cy.get('[name="privacy_policy_accepted"]').click()
+
+      cy.get('[data-cy=next]').click()
+
+    })
+
+  })
+
 })
