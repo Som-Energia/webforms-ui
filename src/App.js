b7460a1e729d1e7362101364d81acbedff8057e1
Benjami <benjami94@gmail.com>
2020-12-02 11:32:18 +0100

    Refactor App.js

--- src/App.js
+++ src/App.js
@@ -51,38 +51,32 @@ const keyMap = {
 }
 
 const App = (props) => {
-  const { d1 = "" } = props
-  let d1_token = ""
   const classes = useStyles()
+  const { d1 = '', token = '' } = props
 
   const loadModifyContract = (props) => {
     const ModifyContract = lazy(() => import('./containers/ModifyContract'))
-    return <ModifyContract {...props} token={d1_token} />
+    return <ModifyContract {...props} token={token} />
   }
 
   const loadD1Detail = (props) => {
     const D1Detail = lazy(() => import('./containers/D1Detail'))
 
-    let d1_data = {}
-    if (typeof d1 === "string" && d1 !== "") {
-      d1_data = JSON.parse(d1)
-    }
-
-    d1_token = d1_data?.token
+    const d1Data = (typeof d1 === 'string' && d1 !== '') ? JSON.parse(d1) : {}
 
     var templateProps = {
-      token: d1_data?.token,
-      installed_power: d1_data?.installed_power,
-      cil: d1_data?.cil,
-      installation_type: d1_data?.installation_type,
-      subsection: d1_data?.subsection,
-      cau: d1_data?.cau,
-      collective: d1_data?.collective === "true",
-      generator_technology: d1_data?.generator_technology,
-      ssaa: d1_data?.ssaa === "true",
-      register_section: d1_data?.register_section,
-      to_validate: d1_data?.to_validate === "true",
-      case_id: d1_data?.case_id,
+      token: d1Data?.token,
+      installed_power: d1Data?.installed_power,
+      cil: d1Data?.cil,
+      installation_type: d1Data?.installation_type,
+      subsection: d1Data?.subsection,
+      cau: d1Data?.cau,
+      collective: d1Data?.collective === 'true',
+      generator_technology: d1Data?.generator_technology,
+      ssaa: d1Data?.ssaa === 'true',
+      register_section: d1Data?.register_section,
+      to_validate: d1Data?.to_validate === 'true',
+      case_id: d1Data?.case_id,
     }
 
     return <D1Detail {...props} templateProps={templateProps} />
