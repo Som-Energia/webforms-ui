2f4db48758bfb3018494003f469a26a6e55f08e4
Marta <marta.fernandez@somenergia.coop>
2021-01-08 13:17:44 +0100

    member vat can not be from a community of owners

--- src/containers/Member.js
+++ src/containers/Member.js
@@ -80,7 +80,9 @@ const Member = (props) => {
   const validationSchemas = [
     Yup.object().shape({
       member: Yup.object().shape({
-        vat: Yup.string().required(t('FILL_NIF')),
+        vat: Yup.string()
+          .required(t('FILL_NIF'))
+          .matches(/(^[A-GI-Z0-9])/, t('FILL_NIF')),
         vatvalid: Yup.bool().required(t('FILL_NIF'))
           .oneOf([true], t('FILL_NIF'))
       })
