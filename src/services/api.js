642df2c63cff7b6ee54f2f683e1b67f39fdc3089
David Riera <david.riera@somenergia.coop>
2021-02-09 18:36:42 +0100

    added api base url as enviroment variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = window?.config?.API_BASE_URL
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || window?.config?.API_BASE_URL
 
 export const modifyContract = async (data) => {
   return axios({
