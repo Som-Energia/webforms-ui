e3330e37f9b0dab0a819f2c57001aeaa6dbb941d
David Riera <david.riera@somenergia.coop>
2021-09-01 15:23:44 +0200

    removed tariff on modify phases post

--- src/services/utils.js
+++ src/services/utils.js
@@ -50,7 +50,7 @@ const normalizeCommonModifyData = (params) => {
       modify?.changePower && modify?.moreThan15Kw
         ? Math.round(modify?.power6 * THOUSANDS_CONVERSION_FACTOR)
         : undefined,
-    tariff: modify?.tariff,
+    tariff: modify?.changePower ? modify?.tariff : undefined,
     contact_name: contact?.contactName,
     contact_surname: contact?.contactSurname,
     contact_phone: contact?.phone
