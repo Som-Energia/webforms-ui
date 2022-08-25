e31dda1250e1d1cb5d532bb6af2fc687fc6994bd
dukenuken09 <gilgassorovira@gmail.com>
2022-08-25 14:08:29 +0200

    Refactor AlertBox component

--- src/components/AlertBox.js
+++ src/components/AlertBox.js
@@ -3,18 +3,19 @@ import {Alert, AlertTitle} from '@material-ui/lab'
 import { Box,Typography } from '@material-ui/core'
 
 
-const AlertBox = ({ severity, title, description }) => {
+const AlertBox = ({ severity, title, description, children }) => {
     
     return (
         <Box mt={0.75} mb={1.5}>
             <Alert severity={severity}>
                 <AlertTitle>{title}</AlertTitle>
-                <Typography
+                {description && <Typography
                     variant="body1"
                     dangerouslySetInnerHTML={{
                         __html: description
                     }}
-                />
+                />}
+                {children}
             </Alert>
         </Box>
     )
