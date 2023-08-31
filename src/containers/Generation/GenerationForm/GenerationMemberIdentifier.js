3c36886758ee305ca6dfe90b1b96730a1e46da3c
dukenuken09 <gilgassorovira@gmail.com>
2023-08-31 11:24:56 +0200

    ♻️ Improve the warning text for no member way

--- src/containers/Generation/GenerationForm/GenerationMemberIdentifier.js
+++ src/containers/Generation/GenerationForm/GenerationMemberIdentifier.js
@@ -26,7 +26,6 @@ const useStyles = makeStyles((theme) => ({
 const GenerationMemberIdentifier = (props) => {
   const { t } = useTranslation()
   const classes = useStyles()
-  
 
   const {
     values,
@@ -40,7 +39,7 @@ const GenerationMemberIdentifier = (props) => {
     resetForm()
     setFieldValue('member.is_member', !!event?.option)
   }
-  
+
   return (
     <>
       <StepHeader title={title} />
@@ -104,29 +103,41 @@ const GenerationMemberIdentifier = (props) => {
                   })
                 }}
               />
+              {!values?.member?.is_member ? (
+                <Typography
+                  variant="body1"
+                  dangerouslySetInnerHTML={{
+                    __html: t('GENERATION_CONTRIBUTION_NO_MEMBER_TO_MEMBER_FORM', {
+                      url: t('GENERATION_CONTRIBUTION_MEMBER_FORM_URL')
+                    })
+                  }}
+                />
+              ) : null}
             </Alert>
           </Grid>
-          <Grid item>
-            <Typography
-              variant="body2"
-              dangerouslySetInnerHTML={{
-                __html: t('GENERATION_FORM_INFO_NOT_VALID_DATA_OF_PARTNER')
-              }}
-            />
-          </Grid>
+          {values?.member?.is_member ? (
+            <Grid item>
+              <Typography
+                variant="body2"
+                dangerouslySetInnerHTML={{
+                  __html: t('GENERATION_FORM_INFO_NOT_VALID_DATA_OF_PARTNER')
+                }}
+              />
+            </Grid>
+          ) : null}
         </Grid>
       ) : null}
 
       {errors?.member?.has_generation_enabled_zone ? (
-        <Grid id="grid_error_enabled_zone" container item >
-            <Alert severity="warning">
-              <Typography
-                variant="body1"
-                dangerouslySetInnerHTML={{
-                  __html: t('GENERATION_FORM_DATA_COULD_NOT_BE_VALIDATED')
-                }}
-              />
-            </Alert>
+        <Grid id="grid_error_enabled_zone" container item>
+          <Alert severity="warning">
+            <Typography
+              variant="body1"
+              dangerouslySetInnerHTML={{
+                __html: t('GENERATION_FORM_DATA_COULD_NOT_BE_VALIDATED')
+              }}
+            />
+          </Alert>
         </Grid>
       ) : null}
     </>
