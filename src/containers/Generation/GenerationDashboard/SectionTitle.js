ea625b4f159b6750ac11d6e29023d329bc5b67c4
dukenuken09 <gilgassorovira@gmail.com>
2023-10-04 11:23:24 +0200

    ✨ Added prod and consumption section in generationkwh dashboard

---
+++ src/containers/Generation/GenerationDashboard/SectionTitle.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { Grid, Typography } from '@material-ui/core'
+
+const useStyles = makeStyles({
+  h2: {
+    background: '#96b633',
+    color: '#fff',
+    textTransform: 'uppercase',
+    fontSize: '14px',
+    margin: '10px 0 4px 0',
+    padding: '1em',
+    display:'flex',
+    justifyContent:'space-between',
+    alignItems:'center'
+  }
+})
+
+function SectionTitle({ text, children }) {
+  const classes = useStyles()
+  return (
+    <Grid container className={classes.h2}>
+      <Typography component="h2">
+        {text}
+      </Typography>
+      {children}
+    </Grid>
+  )
+}
+
+export default SectionTitle
