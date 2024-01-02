837ec072fa12998d17eb49b2b7728f2e1389cae5
David García Garzón <david.garcia@somenergia.coop>
2024-01-02 16:43:37 +0100

    ♻️ Moved validation logic inside the component
    Co-authored-by: Marite Guerrieri <marite.guerrieri@somenergia.coop>

--- src/components/IBANField.js
+++ src/components/IBANField.js
@@ -5,11 +5,12 @@ import { makeStyles } from '@material-ui/core/styles'
 import { checkIban } from '../services/api'
 
 import CircularProgress from '@material-ui/core/CircularProgress'
+import Container from '@material-ui/core/Container'
 import InputAdornment from '@material-ui/core/InputAdornment'
 import TextField from '@material-ui/core/TextField'
-
 import AccountBalanceOutlinedIcon from '@material-ui/icons/AccountBalanceOutlined'
 import CheckOutlinedIcon from '@material-ui/icons/CheckOutlined'
+import { useTranslation } from 'react-i18next'
 
 const useStyles = makeStyles((theme) => ({
   icon: {
@@ -19,59 +20,62 @@ const useStyles = makeStyles((theme) => ({
   }
 }))
 
-function ApiValidatedField(props) {
+function ApiValidatedField({
+  name,
+  id,
+  label,
+  variant,
+  value = '',
+  onChange,
+  onBlur,
+  error,
+  helperText,
+  errorText,
+  autoFocus = false,
+  leadingIcon
+}) {
   const classes = useStyles()
-  const {
-    name,
-    id,
-    label,
-    variant,
-    value = '',
-    onChange,
-    onBlur,
-    error,
-    helperText,
-    autoFocus = false
-  } = props
 
   const [isLoading, setIsLoading] = useState(false)
-  const [isValidIBAN, setIsValidIBAN] = useState(false)
-  const [IBAN, setIBAN] = useState(value)
+  const [isValid, setIsValid] = useState(false)
+  const [currentValue, setCurrentValue] = useState(value)
+  const { t } = useTranslation()
 
-  const handleChangeIBAN = (event) => {
+  const LeadingIcon = leadingIcon
+  const handleChange = (event) => {
     let value = event.target.value
     if (value) {
-      value = value.match(/[\s0-9A-Za-z]{0,29}/)
-      value = value[0].toUpperCase()
+      value = value.match(/[\s0-9A-Za-z]{0,29}/)[0]
+      value = value.toUpperCase()
       value = value.split(' ').join('')
       value = value.match(/.{1,4}/g).join(' ')
     }
-    setIBAN(value)
+    setCurrentValue(value)
   }
+  const hasError = currentValue && !isValid
 
   useEffect(() => {
-    onChange({ IBAN: IBAN, IBANValid: false })
-    if (IBAN.length > 28) {
-      setIsLoading(true)
-      checkIban(IBAN)
-        .then((response) => {
-          onChange({ IBAN: IBAN, IBANValid: response?.state === true })
-          setIsValidIBAN(response?.state === true)
-          setIsLoading(false)
-        })
-        .catch((error) => {
-          const errorStatus = error?.response?.data?.state
-            ? error?.response?.data?.state
-            : false
-          setIsValidIBAN(errorStatus)
-          setIsLoading(false)
-          onChange({ IBAN: IBAN, IBANValid: errorStatus })
-        })
-    } else {
-      onChange({ IBAN: IBAN, IBANValid: false })
-      setIsValidIBAN(false)
+    onChange({ value: currentValue, valid: false })
+    if (currentValue.length <= 28) {
+      setIsValid(false)
+      return
     }
-  }, [IBAN])
+    setIsLoading(true)
+    checkIban(currentValue)
+      .then((response) => {
+        onChange({ value: currentValue, valid: response?.state === true })
+        setIsValid(response?.state === true)
+        setIsLoading(false)
+      })
+      .catch((error) => {
+        const errorStatus = error?.response?.data?.state
+          ? error?.response?.data?.state
+          : false
+        setIsValid(errorStatus)
+        setIsLoading(false)
+        onChange({ value: currentValue, valid: errorStatus })
+      })
+  }, [currentValue])
 
   return (
     <>
@@ -83,23 +87,21 @@ function ApiValidatedField(props) {
         fullWidth
         required
         autoFocus={autoFocus}
-        value={value}
-        onChange={handleChangeIBAN}
+        value={currentValue}
+        onChange={handleChange}
         onBlur={onBlur}
-        error={error}
-        helperText={helperText}
+        error={hasError}
+        helperText={hasError ? errorText ?? t('INVALID_FORMAT') : helperText}
         InputProps={{
-          startAdornment: (
+          startAdornment: LeadingIcon && (
             <InputAdornment className={classes.icon} position="start">
-              <AccountBalanceOutlinedIcon />
+              <LeadingIcon />
             </InputAdornment>
           ),
           endAdornment: (
             <InputAdornment position="end">
               {isLoading && <CircularProgress size={24} />}
-              {!isLoading && isValidIBAN && (
-                <CheckOutlinedIcon color="primary" />
-              )}
+              {!isLoading && isValid && <CheckOutlinedIcon color="primary" />}
             </InputAdornment>
           )
         }}
@@ -109,7 +111,57 @@ function ApiValidatedField(props) {
 }
 
 function IBANField(props) {
-  return <ApiValidatedField {...props} />
+  const { t } = useTranslation()
+  const { onChange, ...others } = props
+  return (
+    <ApiValidatedField
+      {...others}
+      leadingIcon={AccountBalanceOutlinedIcon}
+      errorText={t('INVALID_IBAN_FORMAT')}
+      onChange={(newValue) => {
+        return onChange({
+          ...newValue,
+          IBAN: newValue.value,
+          IBANValid: newValue.valid
+        })
+      }}
+    />
+  )
+}
+export function Test() {
+  const [value, setValue] = React.useState()
+  const [valid, setValid] = React.useState()
+  const [value2, setValue2] = React.useState()
+  const [valid2, setValid2] = React.useState()
+  return (
+    <>
+      <Container>
+        <IBANField
+          value={value}
+          onChange={(newValue) => {
+            setValue(newValue.IBAN)
+            setValid(newValue.IBANValid)
+          }}
+          error={(value || valid) && value !== undefined}
+          helperText={
+            (value !== undefined && (value || valid)) || 'Esto es un campo IBAN'
+          }
+        />
+        <div>Value: {value}</div>
+        <div>Valid: {'' + valid}</div>
+        <IBANField
+          value={value2}
+          onChange={(newValue) => {
+            setValue2(newValue.IBAN)
+            setValid2(newValue.IBANValid)
+          }}
+          helperText={'Esto es un altre camp IBAN'}
+        />
+        <div>Value: {value2}</div>
+        <div>Valid: {'' + valid2}</div>
+      </Container>
+    </>
+  )
 }
 
 export default IBANField
