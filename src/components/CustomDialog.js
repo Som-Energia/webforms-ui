3ad15422fef5c9eb64e3e2a3ac25bee4e0cf9e77
dukenuken09 <gilgassorovira@gmail.com>
2024-03-25 15:10:30 +0100

    ✨ Added the background option as a parameter

--- src/components/CustomDialog.js
+++ src/components/CustomDialog.js
@@ -8,31 +8,36 @@ const useStyles = makeStyles({
   customBackdrop: {
     backgroundColor: 'rgba(150, 182, 51, 0.5)', // Set your desired backdrop color here
   },
-  modal:{
-    position:'absolute',
-    top:0
-  }
+  modal: (withBackground) => ({
+    position: 'absolute',
+    top: 0,
+    backgroundColor: withBackground ? 'white' : 'transparent',
+    boxShadow: withBackground ? '0px 11px 15px -7px rgba(0,0,0,0.2),0px 24px 38px 3px rgba(0,0,0,0.14),0px 9px 46px 8px rgba(0,0,0,0.12)' : 'none'
+  })
 })
 
 
 
-function CustomDialog({children}){
+function CustomDialog({ children, withBackground, blockHandleClose }) {
 
-  const classes = useStyles()
+  const classes = useStyles(withBackground)
   const { setContent } = React.useContext(PopUpContext)
 
   const handleClose = () => {
-    setContent(undefined);
+    if(!blockHandleClose){
+      setContent(undefined);
+    }
   };
 
   return (
     <Dialog
       onClose={handleClose}
-      open={children}
+      open={children === undefined ? false : true}
       PaperProps={{ className: classes.modal }}
       BackdropProps={{ className: classes.customBackdrop }}
+      id='custom-dialog'
     >
-    {children}
+      {children}
     </Dialog>
   );
 }
