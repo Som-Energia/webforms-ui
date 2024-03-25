abcdb09483eda0f4f3d3bbc5623e769c09c61d2b
dukenuken09 <gilgassorovira@gmail.com>
2024-03-25 15:17:40 +0100

    ✨ Added the option to add contracts to the assignments list

--- src/containers/Generation/GenerationDashboard/GenerationDashboard.js
+++ src/containers/Generation/GenerationDashboard/GenerationDashboard.js
@@ -1,14 +1,21 @@
-import React, { useCallback, useContext } from 'react'
+import React, { useCallback, useContext, useState, useEffect } from 'react'
 import SectionTitle from './SectionTitle'
 import GenerationAssigmentSection from './GenerationAssignmentSection'
 import GenerationInvestmentSection from './GenerationInvestmentSection'
 import { Button, Grid, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import GenerationContext from '../context/GenerationContext'
+import PopUpContext from '../../../context/PopUpContext'
 import { useTranslation } from 'react-i18next'
 import Alert from '@material-ui/lab/Alert'
 import GenerationFailure from './GenerationFailure'
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos'
+import AddIcon from '@material-ui/icons/Add';
+import ContractList from '../../../components/ClickableList';
+import SimpleDialog from '../../../components/SimpleDialog';
+import { addContractsToAssignments } from '../../../services/api'
+import Loading from 'components/Loading'
+import CustomDialog from 'components/CustomDialog'
 
 const useStyles = makeStyles({
   footer: {
@@ -18,7 +25,7 @@ const useStyles = makeStyles({
     background: '#96b633',
     color: '#fff'
   },
-  altButton:{
+  altButton: {
     background: '#8E8C8C',
     color: '#fff'
   },
@@ -41,14 +48,21 @@ function GenerationDashboard({
   validationConfirm,
   setValidationConfirm
 }) {
+
   const { t } = useTranslation()
   const classes = useStyles()
   const {
     editingPriority,
     assignments,
     investments,
-    has_duplicate_priority_values
+    has_duplicate_priority_values,
+    getAssingments,
+    getContractsToAssign,
+    contractNoAssignments
   } = useContext(GenerationContext)
+
+  const { setContent } = useContext(PopUpContext)
+  const [loading, setLoading] = useState(false)
 
   const ActionSection = useCallback(() => {
     return (
@@ -99,6 +113,42 @@ function GenerationDashboard({
     validateChanges
   ])
 
+  useEffect(() => {
+    getContractsToAssign()
+  }, [])
+
+
+  const addContracts = async (ids) => {
+    try {
+      setLoading(true)
+      setContent(<CustomDialog withBackground={false} blockHandleClose={true}><Loading /></CustomDialog>)
+      await addContractsToAssignments(ids)
+      await getContractsToAssign()
+      setLoading(false)
+      getAssingments()
+    }
+    catch (e) {
+      console.log(e)
+    }
+    setContent(undefined)
+  }
+
+  const contractListDialog = () => {
+
+    const dataList = contractNoAssignments.map(element => ({ id: element.id, primary: element.contract, secondary: element.contract_address }))
+    if (contractNoAssignments.length === 0) {
+      setContent(
+        <SimpleDialog text={<Typography dangerouslySetInnerHTML={{ __html: t('GENERATION_ADD_ASSIGNMENTS_INFO_NO_CONTRACTS_TEXT') }} />} acceptFunction={() => setContent(undefined)} />
+      )
+    } else {
+      setContent(
+        <CustomDialog withBackground={true} ><ContractList data={dataList} title={t('GENERATION_ADD_CONTRACT_LIST_TITLE')} acceptFunction={addContracts} cancelFunction={() => setContent(undefined)} acceptButtonText={t('GENERATION_ADD_CONTRACT_LIST_VALIDATE_CHANGES')} cancelButtonText={t('GENERATION_ADD_CONTRACT_LIST_CANCEL')} /></CustomDialog>
+      )
+    }
+
+  }
+
+
   return (
     <>
       {validationConfirm.finished && !validationConfirm.completed ? (
@@ -125,9 +175,18 @@ function GenerationDashboard({
                   />
                 </Alert>
               </Grid>
-              <SectionTitle
-                text={t('GENERATION_INVESTMENTS_ASSIGNMENTS_TABLE_TITLE')}
-              />
+              <SectionTitle text={t('GENERATION_INVESTMENTS_ASSIGNMENTS_TABLE_TITLE')}>
+                {investments.length > 0 ? <Button
+                  className={classes.altButton}
+                  disabled={loading}
+                  type="button"
+                  id="generationkwh-id-add-assignment"
+                  variant="contained"
+                  onClick={() => contractListDialog()}
+                  endIcon={<AddIcon />}>
+                  {t('GENERATION_BUTTON_ADD_ASSIGNMENT')}
+                </Button> : null}
+              </SectionTitle>
               <GenerationAssigmentSection
                 data={assignments}
                 editing={editing}
@@ -144,7 +203,7 @@ function GenerationDashboard({
                   type="button"
                   id="generationkwh-id-production-consumption"
                   variant="contained"
-                  onClick={()=> window.location.replace('/investments/production-consumption')}
+                  onClick={() => window.location.replace('/investments/production-consumption')}
                   endIcon={<ArrowForwardIosIcon />}>
                   {t('GENERATION_BUTTON_ACCESS_PRODUCTION_CONSUMPTION')}
                 </Button>
