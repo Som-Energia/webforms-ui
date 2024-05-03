c26a35c8f39248df21aeb03d31d0251606a8373e
dukenuken09 <gilgassorovira@gmail.com>
2024-05-03 14:19:12 +0200

    ♻ 🎨 Migrate mui4 to mui5 for Generation Form, Generation dashboard and generic components

--- src/containers/Indexed/IndexedReviewField.js
+++ src/containers/Indexed/IndexedReviewField.js
@@ -1,36 +1,28 @@
 import clsx from 'clsx'
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import Typography from '@material-ui/core/Typography'
-
-const useStyles = makeStyles((theme) => ({
-  field: {
-    display: 'flex',
-    alignItems: 'center',
-    marginBottom: theme.spacing(0.8),
-    '& .field__value': {
-      flexGrow: 1
-    }
-  },
-  label: {
-    textTransform: 'uppercase',
-    paddingRight: '12px',
-    fontSize: '13px',
-    fontWeight: 400,
-    color: 'rgba(0, 0, 0, 0.54)'
-  },
-  value: {
-    fontSize: '13px'
-  }
-}))
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
 
 const ReviewField = ({ label, value }) => {
-  const classes = useStyles()
+  
   return (
-    <div className={clsx(classes.field)}>
+    <Box sx={{
+      display: 'flex',
+      alignItems: 'center',
+      mb: 0.8,
+      '& .field__value': {
+        flexGrow: 1
+      }
+    }} >
       {label !== false && (
         <div className="field__title">
-          <Typography className={classes.label} variant="subtitle2">
+          <Typography sx={{
+            textTransform: 'uppercase',
+            paddingRight: '12px',
+            fontSize: '13px',
+            fontWeight: 400,
+            color: 'rgba(0, 0, 0, 0.54)'
+          }} variant="subtitle2">
             <>{label}</>
           </Typography>
         </div>
@@ -38,12 +30,12 @@ const ReviewField = ({ label, value }) => {
       <div className={clsx('field__value')}>
         <Typography
           id={label + '__value'}
-          className={classes.value}
+          sx={{ fontSize: '13px' }}
           variant="body2">
           <>{value}</>
         </Typography>
       </div>
-    </div>
+    </Box>
   )
 }
 
