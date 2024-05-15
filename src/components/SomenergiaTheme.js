91b3da584d387aa97ec2bcc6eba498ed7a3469e1
joana <joana.figueira@somenergia.coop>
2024-05-15 13:25:12 +0200

    ✨ 🚧 Add Som Energia Theme

---
+++ src/components/SomenergiaTheme.js
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material'
+
+const SomenergiaTheme = createTheme({
+  palette: {
+    text: {
+      primary: '#4d4d4d',
+    },
+    primary: {
+      main: '#BAC92A',
+      secondary: '#96b633',
+    },
+    secondary: {
+      main: '#666666',
+    },
+  },
+})
+
+export default SomenergiaTheme
