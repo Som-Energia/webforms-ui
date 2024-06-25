b0221b6a83928aeb59c52ea134c24354266e3045
dukenuken09 <gilgassorovira@gmail.com>
2024-06-25 13:00:15 +0200

    ✨ Improve styles as muiv5

--- src/components/Chooser.js
+++ src/components/Chooser.js
@@ -6,13 +6,13 @@ import Grid from '@mui/material//Grid'
 import Radio from '@mui/material/Radio'
 import RadioGroup from '@mui/material/RadioGroup'
 import Typography from '@mui/material/Typography'
-import InputLabel from '@mui/material/InputLabel';
+
 
 const Chooser = (props) => {
   const {
     question,
     options,
-    onChange,
+    onChange = (event) => console.log('change', event.value),
     value,
     disabled,
     name,
@@ -21,58 +21,50 @@ const Chooser = (props) => {
   } = props
   const [selectedOption, setSelectedOption] = useState(value)
 
-  const getStyles = (selected) => {
-    const customStyles = {
-      chooserItem: {
-        cursor: 'pointer',
-        minHeight: '100px',
-        height: '100%',
-        pt: 1,
-        pb: 1,
-        pr: 3,
-        pl: 3,
+  const customStyles = {
+    chooserItem: {
+      cursor: 'pointer',
+      minHeight: '100px',
+      height: '100%',
+      pt: 1,
+      pb: 1,
+      pr: 3,
+      pl: 3,
+      border: '1px solid rgba(0, 0, 0, 0.12)',
+      margin: '0 0 8px 0',
+      '&:hover': {
+        border: '1px solid rgba(0, 0, 0, 0.87)',
+        backgroundColor: 'rgba(0, 0, 0, 0.03)'
+      },
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'flex-start'
+    },
+    chooserItemCondensed: {
+      minHeight: '40px',
+      pt: 1,
+      pb: 1
+    },
+    chooserItemSelected: {
+      margin: '0 0 8px 0',
+      border: '1px solid',
+      borderColor: 'primary.main',
+      backgroundColor: 'primary.extraLight',
+      '&:hover': {
+        border: '1px solid',
+        borderColor: 'primary.main',
+        backgroundColor: 'primary.extraLight'
+      }
+    },
+    chooserItemDisabled: {
+      cursor: 'not-allowed',
+      color: 'rgba(0, 0, 0, 0.54)',
+      '&:hover': {
         border: '1px solid rgba(0, 0, 0, 0.12)',
-        margin: '0 0 8px 0',
-        '&:hover': {
-          border: '1px solid rgba(0, 0, 0, 0.87)',
-          backgroundColor: 'rgba(0, 0, 0, 0.03)'
-        },
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'flex-start'
-      },
-      chooserItemCondensed: {
-        minHeight: '40px',
-        pt: 1,
-        pb: 1
-      },
-      chooserItemSelected: {
-        margin: '0 0 8px 0',
-        border: '1px solid #96b633',
-        backgroundColor: 'rgba(150, 182, 51, 0.08)',
-        '&:hover': {
-          border: '1px solid #96b633',
-          backgroundColor: 'rgba(150, 182, 51, 0.08)'
-        }
-      },
-      chooserItemDisabled: {
-        cursor: 'not-allowed',
-        color: 'rgba(0, 0, 0, 0.54)',
-        '&:hover': {
-          border: '1px solid rgba(0, 0, 0, 0.12)',
-          backgroundColor: 'inherit'
-        }
-      },
+        backgroundColor: 'inherit'
+      }
     }
-
-    let customStylesArray = []
-
-    customStylesArray.push(customStyles.chooserItem)
-    condensed && customStylesArray.push(customStyles.chooserItemCondensed)
-    disabled && customStylesArray.push(customStyles.chooserItemDisabled)
-    selected && customStylesArray.push(customStyles.chooserItemSelected)
-    return customStylesArray
   }
 
 
@@ -99,16 +91,21 @@ const Chooser = (props) => {
         dangerouslySetInnerHTML={{ __html: question }}
       />
       <RadioGroup sx={{ mt: 3 }} defaultValue="" name={name} data-cy={name}>
-        <Grid container spacing={3}>
+        <Grid container spacing={3} >
           {options.map((option, index) => (
             <Grid key={index} item xs={12} sm={6}>
-              <InputLabel
+              <Box
                 data-value={option.value}
                 id={option.id || option.value}
                 onClick={(event) =>
                   !disabled && handleClick(event, option.value)
                 }
-                sx={getStyles(selectedOption === option.value)}>
+                sx={[
+                  customStyles.chooserItem,
+                  condensed && customStyles.chooserItemCondensed,
+                  selectedOption === option.value && customStyles.chooserItemSelected,
+                  disabled && customStyles.chooserItemDisabled
+                ]}>
                 <Box sx={{ display: 'flex', alignItems: 'center' }}>
                   <Radio
                     disabled={disabled}
@@ -124,7 +121,7 @@ const Chooser = (props) => {
                     dangerouslySetInnerHTML={{ __html: option.description }}
                   />
                 )}
-              </InputLabel>
+              </Box>
               {option.helper}
             </Grid>
           ))}
@@ -132,10 +129,6 @@ const Chooser = (props) => {
       </RadioGroup>
     </>
   )
-}
-
-Chooser.defaultProps = {
-  onChange: (event) => console.log('change', event.value)
 }
 
 export default Chooser
