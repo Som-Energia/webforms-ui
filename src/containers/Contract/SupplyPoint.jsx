9ae235a2582ba7e6403b3292e753d5e89ce403f5
joana <joana.figueira@somenergia.coop>
2024-06-27 12:44:46 +0200

    ✨ Enable google address autocomplete
    Co-authored-by: Marta <marta.fernandez@somenergia.coop>

--- src/containers/Contract/SupplyPoint.jsx
+++ src/containers/Contract/SupplyPoint.jsx
@@ -19,6 +19,10 @@ import CnaeField from '../../components/CnaeField'
 import { CNAE_HOUSING } from '../../services/utils'
 import { getMunicipisByPostalCode } from '../../services/api'
 import CadastralReferenceField from '../../components/CadastralReferenceField'
+import LocationInput from '../../components/AddressAutocompletedField'
+
+const SHOW_STATE_CITY = false
+
 
 const CadastralReferenceHelperText = () => {
   const { t } = useTranslation()
@@ -35,6 +39,8 @@ const CadastralReferenceHelperText = () => {
 
 const SupplyPoint = (props) => {
   const { t } = useTranslation()
+
+  const [addressValue, setAddressValue] = useState('')
 
   const {
     values,
@@ -82,7 +88,7 @@ const SupplyPoint = (props) => {
     setFieldValue('supply_point.cnae_valid', valid)
   }
 
-  const onChangeCadastralReference= ({value, valid, error}) => {
+  const onChangeCadastralReference = ({ value, valid, error }) => {
     setFieldValue('supply_point.cadastral_reference', value)
     setFieldValue('supply_point.cadastral_reference_error', error)
   }
@@ -101,6 +107,23 @@ const SupplyPoint = (props) => {
     }
   }, [values?.supply_point?.postal_code])
 
+  const handleAddressChange = (value) => {
+    setAddressValue(value)
+  }
+  const handleLocationSelected = (selection) => {
+    const address =
+      selection !== null
+        ? Object.assign(
+            {},
+            ...selection.address_components.map((x) => ({
+              [x.types[0]]: x.long_name
+            }))
+          )
+        : {}
+    setFieldValue('supply_point.address', address?.route)
+    setFieldValue('supply_point.postal_code', address?.postal_code || '')
+    setFieldValue('supply_point.number', address?.street_number || '')
+  }
   return (
     <>
       <StepHeader title={t('SUPPLY')} />
@@ -111,22 +134,12 @@ const SupplyPoint = (props) => {
       <Box mt={3} mb={1}>
         <Grid container spacing={3}>
           <Grid item xs={12} sm={12}>
-            <TextField
+            <LocationInput
               id="supply_point_address"
               name="supply_point.address"
-              label={t('HOLDER_ADDRESS')}
-              required
-              variant="outlined"
-              fullWidth
-              value={values?.supply_point?.address}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              error={
-                errors?.supply_point?.address && touched?.supply_point?.address
-              }
-              helperText={
-                touched?.supply_point?.address && errors?.supply_point?.address
-              }
+              value={addressValue}
+              onChange={handleAddressChange}
+              onLocationSelected={handleLocationSelected}
             />
           </Grid>
           <Grid item xs={4} sm={2}>
@@ -187,6 +200,7 @@ const SupplyPoint = (props) => {
 
           <Grid item xs={12} sm={6}>
             <TextField
+              disabled={true}
               id="supply_point_postal_code"
               name="supply_point.postal_code"
               label={t('HOLDER_POSTALCODE')}
@@ -207,27 +221,30 @@ const SupplyPoint = (props) => {
             />
           </Grid>
 
-          <StateCity
-            stateId="supply_point_state"
-            stateName="supply_point.state"
-            stateInitial={values?.supply_point?.state}
-            stateError={
-              errors?.supply_point?.state && touched?.supply_point?.state
-            }
-            stateHelperText={
-              touched?.supply_point?.state && errors?.supply_point?.state
-            }
-            cityId="supply_point_city"
-            cityName="supply_point.city"
-            cityInitial={values?.supply_point?.city}
-            cityError={
-              errors?.supply_point?.city && touched?.supply_point?.city
-            }
-            cityHelperText={
-              touched?.supply_point?.city && errors?.supply_point?.city
-            }
-            onChange={onChangeStateCity}
-          />
+          {SHOW_STATE_CITY && (
+            <StateCity
+              disabled={true}
+              stateId="supply_point_state"
+              stateName="supply_point.state"
+              stateInitial={values?.supply_point?.state}
+              stateError={
+                errors?.supply_point?.state && touched?.supply_point?.state
+              }
+              stateHelperText={
+                touched?.supply_point?.state && errors?.supply_point?.state
+              }
+              cityId="supply_point_city"
+              cityName="supply_point.city"
+              cityInitial={values?.supply_point?.city}
+              cityError={
+                errors?.supply_point?.city && touched?.supply_point?.city
+              }
+              cityHelperText={
+                touched?.supply_point?.city && errors?.supply_point?.city
+              }
+              onChange={onChangeStateCity}
+            />
+          )}
 
           <Grid item xs={12} sm={6}>
             <TextField
@@ -270,31 +287,38 @@ const SupplyPoint = (props) => {
               }
               helperText={
                 (touched?.supply_point?.cnae &&
-                  errors?.supply_point?.cnae_valid) || t('HELP_POPOVER_CNAE')
+                  errors?.supply_point?.cnae_valid) ||
+                t('HELP_POPOVER_CNAE')
               }
             />
           </Grid>
           {props.isCadastralReference ? (
-          <Grid item xs={6} sm={6}>
-            <CadastralReferenceField
-              id="supply_point_cadastral_reference"
-              name="supply_point.cadastral_reference"
-              label={t('CADASTRAL_REFERENCE')}
-              variant="outlined"
-              fullWidth
-              value={values?.supply_point?.cadastral_reference}
-              onChange={onChangeCadastralReference}
-              onBlur={handleBlur}
-              error={values?.supply_point?.cadastral_reference && !!errors?.supply_point?.cadastral_reference}
-              helperText={
-                values?.supply_point?.cadastral_reference && errors?.supply_point?.cadastral_reference
-                ?errors?.supply_point?.cadastral_reference
-                :<CadastralReferenceHelperText/>
-              }
-            />
-          </Grid>
-          ): null}
-          <Grid item xs={12} sx={{pt: 0}} >
+            <Grid item xs={6} sm={6}>
+              <CadastralReferenceField
+                id="supply_point_cadastral_reference"
+                name="supply_point.cadastral_reference"
+                label={t('CADASTRAL_REFERENCE')}
+                variant="outlined"
+                fullWidth
+                value={values?.supply_point?.cadastral_reference}
+                onChange={onChangeCadastralReference}
+                onBlur={handleBlur}
+                error={
+                  values?.supply_point?.cadastral_reference &&
+                  !!errors?.supply_point?.cadastral_reference
+                }
+                helperText={
+                  values?.supply_point?.cadastral_reference &&
+                  errors?.supply_point?.cadastral_reference ? (
+                    errors?.supply_point?.cadastral_reference
+                  ) : (
+                    <CadastralReferenceHelperText />
+                  )
+                }
+              />
+            </Grid>
+          ) : null}
+          <Grid item xs={12} sx={{ pt: 0 }}>
             {values?.contract?.has_service ? (
               <Typography>{t('ADJUNTAR_ULTIMA_FACTURA')}</Typography>
             ) : (
