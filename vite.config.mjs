9e1712f94c7c0a78bf89688e22a64e711619fab6
David García Garzón <david.garcia@somenergia.coop>
2024-07-12 22:32:10 +0200

    🏗️ webforms.config reads .envs BASE_URL
    this way we do not need to specify it on package.json scripts

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,35 +1,39 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react'
 import pkg from './package.json'
 import { splitVendorChunkPlugin } from 'vite'
 
-export default defineConfig({
-  resolve: {
-    dedupe: Object.keys(pkg.dependencies)
-  },
-  plugins: [
-    react(),
-    splitVendorChunkPlugin(),
-    // TODO: to be activated after fixing the issues
-    // eslint()
-  ],
-  build: {
-    outDir: 'build',
-    manifest: 'asset-manifest.json',
-    rollupOptions: {
-      output: {
-      }
+export default defineConfig(({mode}) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd(), 'BASE_URL')}
+  return {
+    base: process.env.BASE_URL,
+    resolve: {
+      dedupe: Object.keys(pkg.dependencies)
     },
-    target: "es2020",
-  },
-  server: {
-    open: true,
-    port: 3000
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    css: true,
-    setupFiles: './src/tests/setupTests.js'
+    plugins: [
+      react(),
+      splitVendorChunkPlugin(),
+      // TODO: to be activated after fixing the issues
+      // eslint()
+    ],
+    build: {
+      outDir: 'build',
+      manifest: 'asset-manifest.json',
+      rollupOptions: {
+        output: {
+        }
+      },
+      target: "es2020",
+    },
+    server: {
+      open: true,
+      port: 3000
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      css: true,
+      setupFiles: './src/tests/setupTests.js'
+    }
   }
 })
