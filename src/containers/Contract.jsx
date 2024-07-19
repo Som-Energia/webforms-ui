82d9fdab663bcd3a62674d16917a7fd1d2199313
David García Garzón <david.garcia@somenergia.coop>
2024-07-19 03:51:09 +0200

    ⚡️ Bye Google Analytics
    Don't track users with GA if we are using Plausible.
    Besides 1% 10K of gzip payload reduced.

--- src/containers/Contract.jsx
+++ src/containers/Contract.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useContext } from 'react'
-import ReactGA from 'react-ga'
 import Plausible from 'plausible-tracker'
 
 import { useTranslation } from 'react-i18next'
@@ -807,9 +806,6 @@ const Contract = (props) => {
         window.location.hostname +
         '/es/contratacion-realizada/'
     })
-
-    ReactGA.initialize(GA_TRACKING_ID)
-    ReactGA.pageview('/es/contratacion-realizada/')
   }
 
   const handlePost = async (values) => {
