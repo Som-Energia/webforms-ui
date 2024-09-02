9767c6b89fece9948b8aff2734cfeb536dde4353
Beatrigz <beatriz.plana@somenergia.coop>
2024-09-02 13:03:19 +0200

    ✅ tests contract_20

--- cypress/integration/contract/contract_20.spec.js
+++ cypress/integration/contract/contract_20.spec.js
@@ -51,4 +51,46 @@ describe('Contract', () => {
 
     })
   })
+
+  describe('Contract with service, no selfconsumption, correct data', function () {
+    beforeEach(function () {
+      cy.identifyMember(this.data.member.number, this.data.member.vat)
+      cy.identifySupplyPoint(
+        this.data.supplyPoint.cups,
+        this.data.supplyPoint.hasService
+      )
+      cy.enterSupplyPointData(this.data.supplyPoint
+      )
+    })
+
+     it('Can not contract 3.0TD', function () {
+      const moreThan15Kw = true
+
+      cy.chooseMoreOrLessThan15Kw(moreThan15Kw)
+
+      cy.get('.MuiAlert-message').contains('no es posible contratar')
+    })
+
+    it('Contract with 2.0TD', function () {
+      const moreThan15Kw = false
+      const powers = [this.data.power, this.data.power2]
+
+      cy.enterPowerFare(moreThan15Kw, powers)
+
+      cy.chooseTariff(this.data.isIndexed)
+
+      cy.enterSelfConsumption(this.data.selfConsumption.have_no_installation)
+
+      cy.identifyOwner(this.data.member.vat, this.data.holder.previousHolder)
+
+      cy.enterVoluntaryCent(this.data.holder.voluntaryCent)
+
+      cy.enterPaymentData(this.data.holder.iban)
+
+      cy.reviewAndConfirmData()
+
+      cy.get('[data-cy=submit]').should('not.have.class', 'Mui-disabled')
+
+    })
+  })
 })
