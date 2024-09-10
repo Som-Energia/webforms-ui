c5624dba10a043bc4372693c92b8a5a3662ee326
marta197 <marta.fernandez@somenergia.coop>
2024-09-10 16:32:54 +0200

    ✨ added SomStepper and Requirements screens for GURB
    Co-authored-by: Joana Figueira <joana.figueira@somenergia.coop>

---
+++ src/containers/Gurb/components/SomStepper.jsx
@@ -0,0 +1,61 @@
+import { textBody3, textSubtitle } from '../gurbTheme'
+import Header from './Header'
+
+import { styled } from '@mui/material/styles'
+import Typography from '@mui/material/Typography'
+import Stepper from '@mui/material/Stepper'
+import Step from '@mui/material/Step'
+import StepConnector, {
+  stepConnectorClasses
+} from '@mui/material/StepConnector'
+import { useEffect, useState } from 'react'
+
+const SomConnector = styled(StepConnector)(({ theme }) => ({
+  marginTop: '2rem',
+  [`&.${stepConnectorClasses.active}`]: {
+    [`& .${stepConnectorClasses.line}`]: {
+      borderColor: '#96B633'
+    }
+  },
+  [`&.${stepConnectorClasses.completed}`]: {
+    [`& .${stepConnectorClasses.line}`]: {
+      borderColor: '#96B633'
+    }
+  },
+  [`& .${stepConnectorClasses.line}`]: {
+    borderColor: '#E6E6E6',
+    borderTopWidth: 6,
+    borderRadius: 10,
+    ...theme.applyStyles('dark', {
+      borderColor: theme.palette.grey[800]
+    })
+  }
+}))
+
+const SomStepper = (props) => {
+  const { step, connectors } = props
+  const [activeStep, setActiveStep] = useState(0)
+
+  useEffect(() => {
+    setActiveStep(step + 1)
+  }, [step])
+
+  return (
+    <>
+      <Typography sx={textSubtitle}>Formulari inscripció</Typography>
+      <Header title="Requisits de participació" />
+      <Typography sx={textBody3}>
+        Per participar has de complir els 4 requisits que et preguntem a
+        continuació
+      </Typography>
+      <Stepper activeStep={activeStep} connector={<SomConnector />}>
+        {Array(connectors)
+          .fill(0)
+          .map((label, index) => {
+            return <Step key={index} />
+          })}
+      </Stepper>
+    </>
+  )
+}
+export default SomStepper
