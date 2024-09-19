d5d510430531d3a05e840d9cf2889d8866a067ac
joana <joana.figueira@somenergia.coop>
2024-09-19 11:03:28 +0200

    🚧 Component FailureRequirement and requirement pages
    Co-authored-by: Marta <marta.fernandez@somenergia.coop>

--- src/containers/Gurb/SupplyPoint.jsx
+++ src/containers/Gurb/SupplyPoint.jsx
@@ -5,6 +5,9 @@ import ProgressWarning from './components/ProgressWarning'
 import TextRecomendation from './components/TextRecomendation'
 import CUPS from './components/CUPS'
 
+import FailureRequirement from './components/FailureRequirement'
+import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined'
+
 const SupplyPoint = (props) => {
   //   const { values, handleChange, setFieldValue } = props
   const { t } = useTranslation()
@@ -20,7 +23,13 @@ const SupplyPoint = (props) => {
         text={t('GURG_SUPPLY_POINT_RECOMENDATION_TEXT')}
       />
       &nbsp;
-      <CUPS/>
+      <CUPS />
+      <FailureRequirement
+        icon={<CancelOutlinedIcon sx={{ color:'#EE4949', fontSize:'50px'}}/>}
+        textHeader={'test'}
+        textBody={'textBody'}
+        textHelper={'textHelper'}
+      />
     </>
   )
 }
