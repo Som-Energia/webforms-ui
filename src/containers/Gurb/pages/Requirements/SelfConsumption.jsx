4e76d6a37cf74cbb3315f313efcf82e3ccfd4862
marta197 <marta.fernandez@somenergia.coop>
2024-10-08 11:36:43 +0200

    ✨ added error when has selfconsumption in gurb
    Co-authored-by: Joana Figueira <joana.figueira@somenergia.coop>

--- src/containers/Gurb/pages/Requirements/SelfConsumption.jsx
+++ src/containers/Gurb/pages/Requirements/SelfConsumption.jsx
@@ -1,3 +1,4 @@
+import { useContext } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import TextRecomendation from '../../components/TextRecomendation'
@@ -8,12 +9,26 @@ import SolarPowerOutlinedIcon from '@mui/icons-material/SolarPowerOutlined'
 
 import { iconRequirements, iconOffRequirements } from '../../gurbTheme'
 
+import GurbErrorContext from '../../../../context/GurbErrorContext'
+
 const SelfConsumption = (props) => {
   const { values, setFieldValue } = props
   const { t } = useTranslation()
+  const { setError, setErrorInfo } = useContext(GurbErrorContext)
 
   const handleSelfconsumptionQuestion = (value) => {
     setFieldValue('has_selfconsumption', value)
+    if (value === 'selfconsumption-on') {
+      setError(true)
+      setErrorInfo({
+        main_text: t('GURB_SELFCONSUMPTION_ERROR_MAIN_TEXT'),
+        seconday_text: t('GURB_SELFCONSUMPTION_ERROR_SECONDARY_TEXT'),
+        link_text: t('GURB_SELFCONSUMPTION_ERROR_LINK_TEXT'),
+        test: () => {
+          setFieldValue('has_selfconsumption', undefined)
+        }
+      })
+    }
   }
   const options = [
     {
