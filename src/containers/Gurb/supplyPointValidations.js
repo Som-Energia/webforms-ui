32f7f2946dec709a599c79a2697ec875502468e9
marta197 <marta.fernandez@somenergia.coop>
2024-10-17 11:05:54 +0200

    🏜️ some improvements in grub form
    * text errors
    * cups validations
    * input field translates the given errors

--- src/containers/Gurb/supplyPointValidations.js
+++ src/containers/Gurb/supplyPointValidations.js
@@ -3,9 +3,9 @@ import * as Yup from 'yup'
 const supplyPointValidations = Yup.object().shape({
   cups: Yup.string()
     // .matches(/^ES[0-9]{15}\d*$/, 'INVALID_FIELD')
-    .min(20, 'TOO_SHORT')
-    .max(22, 'TOO_LONG')
-    .required('REQUIRED_FIELD')
+    .min(20, 'ERROR_FIELD_TOO_SHORT')
+    .max(22, 'ERROR_FIELD_TOO_LONG')
+    .required('ERROR_REQUIRED_FIELD')
 })
 
 export default supplyPointValidations
