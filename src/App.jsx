29241e3349a811f786bca91bed59f5d190bfbb4b
marta197 <marta.fernandez@somenergia.coop>
2024-10-17 11:16:09 +0200

    🐛 ups! added a missing import
    * added GurbLoadingContextProvider import in App

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import './App.css'
 import { GenerationContextProvider } from './containers/Generation/context/GenerationContext'
 import { PopUpContextProvider } from './context/PopUpContext'
 import { GurbErrorContextProvider } from './context/GurbErrorContext'
+import { GurbLoadingContextProvider } from './context/GurbLoadingContext'
 
 const App = (props) => {
   const { token = '', isIndexedPilotOngoing = undefined } = props
