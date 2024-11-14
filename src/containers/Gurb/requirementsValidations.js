1d3c9b0b3b4477a3f17172a52fa153bdaa23599a
joana <joana.figueira@somenergia.coop>
2024-11-14 09:38:31 +0100

    🧹 remove unused address yuo validations

--- src/containers/Gurb/requirementsValidations.js
+++ src/containers/Gurb/requirementsValidations.js
@@ -2,36 +2,24 @@ import * as Yup from 'yup'
 
 export const lightValidations = Yup.object().shape({
   has_light: Yup.string()
-  .required('REQUIRED_FIELD')
-  .oneOf(['light-on', 'light-off'])
+    .required('REQUIRED_FIELD')
+    .oneOf(['light-on', 'light-off'])
 })
 
 export const addressValidations = Yup.object().shape({
   address: Yup.object().shape({
-    street: Yup.string().required('NO_ADDRESS'),
-    // number: Yup.string().required('NO_NUMBER'),
-    // postal_code: Yup.string()
-    //   .matches(/^\d*$/, 'NO_POSTALCODE')
-    //   .required('NO_POSTALCODE')
-    //   .min(5, 'NO_POSTALCODE')
-    //   .max(5, 'NO_POSTALCODE'),
-    // state: Yup.object().shape({
-    //   id: Yup.number().required('NO_STATE')
-    // }),
-    // city: Yup.object().shape({
-    //   id: Yup.number().required('NO_CITY')
-    // })
+    street: Yup.string().required('NO_ADDRESS')
   })
 })
 
 export const selfConsumptionValidations = Yup.object().shape({
   has_selfconsumption: Yup.string()
-  .required('REQUIRED_FIELD')
-  .oneOf(['selfconsumption-on', 'selfconsumption-off'])
+    .required('REQUIRED_FIELD')
+    .oneOf(['selfconsumption-on', 'selfconsumption-off'])
 })
 
 export const memberQuestionValidations = Yup.object().shape({
   has_member: Yup.string()
-  .required('REQUIRED_FIELD')
-  .oneOf(['member-on', 'member-off', 'apadrinating'])
+    .required('REQUIRED_FIELD')
+    .oneOf(['member-on', 'member-off', 'apadrinating'])
 })
