da2b109bf2a20a21229a561bbd193f25d3ed709c
joana <joana.figueira@somenergia.coop>
2024-12-02 16:34:57 +0100

    🚧  Add new member pages

---
+++ src/containers/Gurb/pages/NewMember/ApadrinatingDetails.jsx
@@ -0,0 +1,98 @@
+import { useContext, useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import InputField from '../../components/InputField'
+import TextRecomendation from '../../components/TextRecomendation'
+
+import { checkMember } from '../../../../services/api'
+import GurbLoadingContext from '../../../../context/GurbLoadingContext'
+
+const ApadrinatingDetails = (props) => {
+  const {
+    values,
+    errors,
+    touched,
+    setFieldValue,
+    setFieldError,
+    setErrors,
+    setFieldTouched
+  } = props
+
+  const { t } = useTranslation()
+  const { loading, setLoading } = useContext(GurbLoadingContext)
+
+  const handleCheckMemberResponse = async () => {
+    let status = undefined
+    await checkMember(values.member.number, values.member.dni)
+      .then((response) => {
+        status = response?.state
+      })
+      .catch(() => {
+        status = false
+      })
+    if (status === true) {
+      await setFieldValue('member.link_member', true)
+      setErrors({})
+    } else {
+      await setFieldValue('member.link_member', false)
+      await setFieldError('link_member', 'WRONG_VALUES')
+      setFieldTouched('member.number', true)
+    }
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    if (values?.member?.dni && values?.member?.number) {
+      handleCheckMemberResponse()
+    }
+  }, [values.member.number, values.member.dni])
+
+  const handleInputDni = (event) => {
+    console.log(event)
+    let value = event.target.value.match(/[0-9A-Za-z]{0,12}/)
+    value = value[0].toUpperCase()
+    setFieldValue('member.dni', value)
+  }
+
+  const handleInputDniBlur = () => {
+    setFieldTouched('member.dni', true)
+  }
+
+  const handleInputMemberNumber = (event) => {
+    let match = event.target.value.replace(/[^0-9]/g, '')
+    setFieldValue('member.number', match)
+  }
+  const handleInputMemberNumberBlur = () => {
+    setFieldTouched('member.number', true)
+  }
+
+  return (
+    <>
+      <TextRecomendation title={t('GURB_APADRINATING_MEMBER_TITLE')} />
+      <InputField
+        textFieldLabel={t('GURB_DNI_LABEL')}
+        textFieldName={t('GURB_DNI_APADRINATING_FIELD')}
+        textFieldHelper={t('GURB_DNI_HELPER')}
+        iconHelper={true}
+        handleChange={handleInputDni}
+        handleBlur={handleInputDniBlur}
+        touched={touched?.member?.dni}
+        value={values?.member.dni}
+        error={errors?.member?.dni}
+      />
+      <InputField
+        textFieldLabel={t('GURB_MEMBER_NUMBER_LABEL')}
+        textFieldName={t('GURB_APADRINATING_MEMBER_NUMBER_FIELD')}
+        textFieldHelper={t('GURB_MEMBER_NUMBER_HELPER')}
+        iconHelper={true}
+        handleChange={handleInputMemberNumber}
+        handleBlur={handleInputMemberNumberBlur}
+        touched={touched?.member?.number}
+        value={values?.member.number}
+        error={errors?.link_member}
+        isLoading={loading}
+      />
+    </>
+  )
+}
+export default ApadrinatingDetails
---
+++ src/containers/Gurb/pages/NewMember/NewMemberDetails.jsx
@@ -0,0 +1,175 @@
+import { useTranslation } from 'react-i18next'
+
+import Typography from '@mui/material/Typography'
+
+import { textHeader4, textSubtitle2 } from '../../gurbTheme'
+import InputField from '../../components/InputField'
+import TextRecomendation from '../../components/TextRecomendation'
+import Box from '@mui/material/Box'
+import MenuItem from '@mui/material/MenuItem'
+
+import TextField from '@mui/material/TextField'
+
+const languages = {
+  es_ES: 'Español',
+  ca_ES: 'Català',
+  eu_ES: 'Euskera',
+  gl_ES: 'Galego'
+}
+
+const NewMemberDetails = (props) => {
+  const {
+    values,
+    errors,
+    touched,
+    setFieldValue,
+    setFieldError,
+    setFieldTouched
+  } = props
+
+  const { t } = useTranslation()
+
+  const handleInputDni = (event) => {
+    let value = event.target.value.match(/^[0-9A-Z][0-9]{7}[0-9A-Z]\d*$/)
+    value = value[0].toUpperCase()
+    setFieldValue('member.dni', value)
+  }
+
+  const handleInputDniBlur = () => {
+    setFieldTouched('member.dni', true)
+  }
+
+  const handleLanguageChange = (event) => {
+    let value = event.target.value
+    setFieldValue('member.language', value)
+  }
+
+  return (
+    <>
+      <TextRecomendation title={t('GURB_NEW_MEMBER_TITLE')} />
+      <InputField
+        textFieldLabel={t('GURB_DNI_LABEL')}
+        textFieldName={t('GURB_DNI_NEW_MEMBER_FIELD')}
+        textFieldHelper={t('GURB_DNI_HELPER')}
+        iconHelper={true}
+        handleChange={handleInputDni}
+        handleBlur={handleInputDniBlur}
+        touched={touched?.member?.dni}
+        value={values?.member.dni}
+        error={errors?.member?.dni}
+      />
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between'
+        }}>
+        <InputField
+          textFieldLabel={t('GURB_NAME_LABEL')}
+          textFieldName={t('GURB_NAME_FIELD')}
+          handleChange={(event) => {
+            setFieldValue('member.name', event.target.value)
+          }}
+          handleBlur={() => {
+            setFieldTouched('member.name', true)
+          }}
+          touched={touched?.member?.name}
+          value={values?.member.name}
+          error={errors?.member?.name}
+        />
+        <InputField
+          textFieldLabel={t('GURB_SURNAME1_LABEL')}
+          textFieldName={t('GURB_SURNAME1_FIELD')}
+          handleChange={(event) => {
+            setFieldValue('member.surname1', event.target.value)
+          }}
+          handleBlur={() => {
+            setFieldTouched('member.surname1', true)
+          }}
+          touched={touched?.member?.surname1}
+          value={values?.member.surname1}
+          error={errors?.member?.surname1}
+        />
+        <InputField
+          textFieldLabel={t('GURB_SURNAME2_LABEL')}
+          textFieldName={t('GURB_SURNAME2_FIELD')}
+          handleChange={(event) => {
+            setFieldValue('member.surname2', event.target.value)
+          }}
+          handleBlur={() => {
+            setFieldTouched('member.surname2', true)
+          }}
+          touched={touched?.member?.surname2}
+          value={values?.member.surname2}
+          error={errors?.member?.surname2}
+        />
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between'
+        }}>
+        <InputField
+          textFieldLabel={t('GURB_EMAIL_LABEL')}
+          textFieldName={t('GURB_EMAIL_FIELD')}
+          handleChange={(event) => {
+            setFieldValue('member.email', event.target.value)
+          }}
+          handleBlur={() => {
+            setFieldTouched('member.email', true)
+          }}
+          touched={touched?.member?.email}
+          value={values?.member.email}
+          error={errors?.member?.email}
+        />
+        <InputField
+          textFieldLabel={t('GURB_EMAIL2_LABEL')}
+          textFieldName={t('GURB_EMAIL2_LABEL')}
+          handleChange={(event) => {
+            setFieldValue('member.email2', event.target.value)
+          }}
+          handleBlur={() => {
+            setFieldTouched('member.email2', true)
+          }}
+          touched={touched?.member?.email2}
+          value={values?.member.email2}
+          error={errors?.member?.email2}
+        />
+      </Box>
+      {/* TO DO: make a component */}
+      <Box sx={{ marginTop: '2rem' }}>
+        <Typography sx={textHeader4}>{t('GURB_LANGUAGE_FIELD')}</Typography>
+        <TextField
+          sx={{
+            '& .MuiFormHelperText-root': { color: '#B3B3B3' },
+            '& .MuiInputLabel-root': { color: '#B3B3B3' },
+            marginTop: '0.5rem'
+          }}
+          select
+          fullWidth
+          InputProps={{
+            sx: { borderRadius: '8px', display: 'flex' }
+          }}
+          onChange={handleLanguageChange}
+          label={t('GURB_LANGUAGE_LABEL')}
+          value={values?.member.language}>
+          {Object.keys(languages).map((id) => (
+            <MenuItem key={id} value={id}>
+              {languages[id]}
+            </MenuItem>
+          ))}
+        </TextField>
+      </Box>
+      <Box sx={{ marginTop: '2rem' }}>
+        <Typography sx={{ ...textSubtitle2, marginBottom: '1rem' }}>
+          {t('GURB_NEW_MEMBER_RIGHTS')}
+        </Typography>
+        <Typography sx={textSubtitle2}>
+          {t('GURB_NEW_MEMBER_THIRD_PERSON_PERSONAL_DATA')}
+        </Typography>
+      </Box>
+    </>
+  )
+}
+export default NewMemberDetails
