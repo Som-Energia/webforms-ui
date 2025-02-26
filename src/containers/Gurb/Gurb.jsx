2dfa7c3090921d176847860a80555deb886c278c
dukenuken09 <gilgassorovira@gmail.com>
2025-02-26 16:11:21 +0100

    ✨ Added all the steps in Gurb step (Gurb Form)

--- src/containers/Gurb/Gurb.jsx
+++ src/containers/Gurb/Gurb.jsx
@@ -1,13 +1,13 @@
 import { useContext } from 'react'
 import { useTranslation } from 'react-i18next'
-
 import SomGurbStepper, { GURB_FINAL_STEP } from './components/SomGurbStepper'
-import Header from './components/Header'
-
 import Typography from '@mui/material/Typography'
-import { textSubtitle } from './gurbTheme'
+import { textSubtitle, textHeader2 } from './gurbTheme'
 import GurbErrorContext from '../../context/GurbErrorContext'
 import GurbParticipation from './pages/Gurb/GurbParticipation'
+import ContractReview from './pages/Gurb/ContractReview'
+import SomStepper from './components/SomStepper'
+import Payment from './pages/Gurb/Payment'
 
 
 
@@ -16,12 +16,27 @@ const Contract = (props) => {
     const { t } = useTranslation()
     const { error, errorInfo, getStepResult } = useContext(GurbErrorContext)
 
+    const getTitle = () => {
+        if (activeStep === 0) {
+            return t('GURB_PARTICIPATION_TITLE_1_STEP')
+        }
+        else if (activeStep === 1){
+            return t('GURB_PARTICIPATION_TITLE_2_STEP')
+        }
+        else {
+            return t('GURB_PARTICIPATION_TITLE_3_STEP')
+        }
+    }
+
     const getStep = () => {
         if (activeStep === 0) {
             return <GurbParticipation {...props} />
         }
+        else if (activeStep === 1){
+            return <ContractReview {...props} />
+        }
         else {
-            return <></>
+            return <Payment {...props} />
         }
     }
 
@@ -29,7 +44,8 @@ const Contract = (props) => {
         <>
             <Typography sx={textSubtitle}>{t('GURB_FORM_TITLE')}</Typography>
             <SomGurbStepper activeStep={GURB_FINAL_STEP} />
-            <Header title={t('GURB_REQUIREMENTS_TITLE')} />
+            <Typography sx={textHeader2}>{getTitle()}</Typography>
+            <SomStepper step={activeStep} connectors={3 + 1} />
             {error ? getStepResult(errorInfo) : getStep()}
         </>
     )
