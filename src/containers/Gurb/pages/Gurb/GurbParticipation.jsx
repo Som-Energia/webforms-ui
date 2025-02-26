f761c87797801662e5f411d1cbb8595a59ed9c17
dukenuken09 <gilgassorovira@gmail.com>
2025-02-26 16:10:14 +0100

    ✨ Added select and info messages in Gurb Participation (Gurb Form)

--- src/containers/Gurb/pages/Gurb/GurbParticipation.jsx
+++ src/containers/Gurb/pages/Gurb/GurbParticipation.jsx
@@ -1,13 +1,12 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import Typography from '@mui/material/Typography'
-import { textHeader4, textField } from '../../gurbTheme'
-import TextField from '@mui/material/TextField'
-import MenuItem from '@mui/material/MenuItem'
+import { textHeader4, textField, textHeader5 } from '../../gurbTheme'
 import Box from '@mui/material/Box'
 import Select from '../../components/Select'
+import Alert from '@mui/material/Alert'
 
-const Contract = (props) => {    
+const Contract = (props) => {
     const options = [
         {
             id: '90-pastel',
@@ -23,14 +22,30 @@ const Contract = (props) => {
     const { t } = useTranslation()
 
     return (
-        <Box sx={{ marginTop: '2rem' }}>
-            <Typography sx={textHeader4}>{t('GURB_LANGUAGE_FIELD')}</Typography>
-            <Select 
-                options = {options}
-                value = {value}
-                handleChange = {setValue}
-                style = {textField}
+        <Box sx={{ display:'flex', flexDirection:'column', gap: 1 }}>
+            <Typography sx={textHeader4}>{t('GURB_PARTICIPATION_KW_INPUT_TEXT')}</Typography>
+            <Typography sx={textHeader5}>{t('GURB_PARTICIPATION_KW_INPUT_TEXT_SECONDARY')}</Typography>
+            <Select
+                options={options}
+                value={value}
+                handleChange={setValue}
+                style={textField}
+                helperText={
+                    <span
+                        dangerouslySetInnerHTML={{
+                            __html: t('GURB_HELP_ANNUAL_CONSUMPTION', {
+                                url: t('GURB_HELP_ANNUAL_CONSUMPTION_URL')
+                            })
+                        }}
+                    />
+                }
             />
+            <Alert severity='info'><Typography variant='body2' align='justify' dangerouslySetInnerHTML={{
+                __html: t('GURB_PARTICIPATION_TEXT_1')
+            }} /> </Alert>
+            <Alert severity='info'><Typography variant='body2' align='justify' dangerouslySetInnerHTML={{
+                __html: t('GURB_PARTICIPATION_TEXT_2')
+            }} /> </Alert>
         </Box>
     )
 }
