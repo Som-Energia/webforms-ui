59d793a76c45855eab07329c39cd628a892b4f99
dukenuken09 <gilgassorovira@gmail.com>
2025-03-11 11:13:35 +0100

    ✨ Mod data used of Select component

--- src/containers/Gurb/components/Select.jsx
+++ src/containers/Gurb/components/Select.jsx
@@ -5,32 +5,31 @@ import { textField } from '../gurbTheme'
 
 
 const Select = (props) => {
-    const { options, value, handleChange, helperText } = props
-
-    return (
-            <TextField
-                data-cy="select_component"
-                sx={textField}
-                required
-                select
-                fullWidth
-                InputProps={{
-                    sx: {
-                        borderRadius: '8px',
-                        display: 'flex'
-                    }
-                }}
-                onChange={(event) => handleChange(event.target.value)}
-                value={value}
-                helperText={helperText}
-                >
-                {options.map((element) => (
-                    <MenuItem data-cy={element.id} key={element.id} value={element.value}>
-                        {element.value}
-                    </MenuItem>
-                ))}
-            </TextField>
-    )
+  const { options, value, handleChange, helperText } = props
+  return (
+    <TextField
+      data-cy="select_component"
+      sx={textField}
+      required
+      select
+      fullWidth
+      InputProps={{
+        sx: {
+          borderRadius: '8px',
+          display: 'flex'
+        }
+      }}
+      onChange={(event) => handleChange(event.target.value)}
+      value={value}
+      helperText={helperText}
+    >
+      {options.map((element) => (
+        <MenuItem data-cy={element.id} key={element.id} value={element.value}>
+          {element.text}
+        </MenuItem>
+      ))}
+    </TextField>
+  )
 }
 
 export default Select
