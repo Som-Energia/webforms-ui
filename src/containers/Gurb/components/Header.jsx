67755ae3b530f93fe831f7ef59d722455360aa22
dukenuken09 <gilgassorovira@gmail.com>
2025-03-25 13:02:24 +0100

    ✨ 🚧 Test cosutomize styles in mui theme

--- src/containers/Gurb/components/Header.jsx
+++ src/containers/Gurb/components/Header.jsx
@@ -1,13 +1,14 @@
 import Typography from '@mui/material/Typography'
 
 import { textHeader1 } from '../gurbTheme'
+import useTheme from '@mui/material/styles/useTheme'
 
 const Header = ({ title }) => {
-
+  const theme = useTheme()
   return (
     <Typography
       // variant="h2"
-      sx={textHeader1}>
+      sx={theme.gurbStyles.textHeader1}>
       {title}
     </Typography>
   )
