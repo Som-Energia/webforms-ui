ff26457a18970aa378ec333cf4fff29818d3158f
marta197 <marta.fernandez@somenergia.coop>
2025-04-10 10:41:24 +0200

    🧹 clean phone field
    Co-authored-by: Marite Guerrieri <marite.guerrieri@somenergia.coop>

--- src/components/PhoneField.jsx
+++ src/components/PhoneField.jsx
@@ -4,13 +4,9 @@ import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined'
 import Grid from '@mui/material/Grid'
-import InputAdornment from '@mui/material/InputAdornment'
-// import AccountCircle from '@mui/icons-material/AccountCircle'
-import HighlightOffIcon from '@mui/icons-material/HighlightOff'
 
 import MuiPhoneNumber from 'mui-phone-number'
 
-import { textHeader4, textHeader5, textHelper1, textField } from '../containers/Gurb/gurbTheme'
 import InputTitle from '../containers/Gurb/components/InputTitle'
 
 export const HelperText = ({
@@ -19,7 +15,7 @@ export const HelperText = ({
   justifyContent = false
 }) => {
   return (
-    <Box display="flex" justifyContent={justifyContent} sx={textHelper1}>
+    <Box display="flex" justifyContent={justifyContent}>
       {iconHelper && (
         <InfoOutlinedIcon sx={{ fontSize: '14px', margin: '2px' }} />
       )}
@@ -42,41 +38,28 @@ const PhoneField = ({
   readonlyField = false,
   required = false,
   name = false
-
 }) => {
   const { t } = useTranslation()
 
   return (
     <Grid container spacing={1}>
       <Grid item xs={12}>
-        <InputTitle
-          text={textFieldName}
-          textStyle={textHeader4}
-          required={required}
-        />
+        <InputTitle text={textFieldName} required={required} />
       </Grid>
       <Grid item xs={12}>
-        <Typography sx={textHeader5}>{textFieldNameHelper}</Typography>
+        <Typography>{textFieldNameHelper}</Typography>
       </Grid>
       <Grid item xs={12}>
         <MuiPhoneNumber
-          variant='outlined'
-          sx={textField}
-          defaultCountry={'es'}
-          disableAreaCodes='True'
-          preferredCountries={['es']}
+          variant="outlined"
+          defaultCountry="es"
+          disableAreaCodes={true}
           name={name}
           data-cy={name}
           disabled={readonlyField}
           fullWidth
           InputProps={{
-            onBlur: handleBlur,
-            startAdornment: (
-                <HighlightOffIcon />
-              // <InputAdornment position="start">
-              //   {/* <AccountCircle /> */}
-              // </InputAdornment>
-            ),
+            onBlur: handleBlur
           }}
           label={value ? undefined : textFieldLabel}
           helperText={
