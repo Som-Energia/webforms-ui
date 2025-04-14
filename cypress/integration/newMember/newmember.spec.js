3ee29a5118449d086143d4d975dcd4d57fb02f72
Marite Guerrieri <marite.guerrieri@somenergia.coop>
2025-04-14 14:40:14 +0200

    ✅ cypress test for legal person

--- cypress/integration/newMember/newmember.spec.js
+++ cypress/integration/newMember/newmember.spec.js
@@ -11,17 +11,33 @@ describe('Member', () => {
     cy.fixture('newMember.json').as('data')
   })
 
-  describe('Modify summary member code ', function () {
-    it('New member', function () {
-      cy.identifyNewMember(this.data.personaldata.nif)
-      cy.personalDataMember(this.data.personaldata, this.data.validAddress)
+  describe('Modify summary physical member code ', function () {
+    it('New physical member', function () {
+      cy.identifyNewMember(this.data.personalPhysicalData.nif)
+      cy.personalPhysicalDataMember(this.data.personalPhysicalData, this.data.validAddress)
       cy.choosePaymentMethod()
-      cy.paymentData(this.data.personaldata.iban)
-      // cy.checkReviewMemberStep(this.personaldata.memberNumber)
+      cy.paymentData(this.data.personalPhysicalData.iban)
+      cy.checkReviewNewMemberStep(this.data.personalPhysicalData.nif)
 
-      // cy.get('button').contains('518').click()
-      // cy.get('[data-cy=next]').click()
-      // cy.checkReviewMemberStep(this.personaldata.memberNumber)
+      cy.get('button').contains('74865726J').click()
+      cy.get('[data-cy=next]').click()
+      cy.get('[data-cy=next]').click()
+      cy.checkReviewNewMemberStep(this.data.personalPhysicalData.nif)
+    })
+  })
+
+  describe('Modify summary legal member code ', function () {
+    it('New legal member', function () {
+      cy.identifyNewMember(this.data.personalLegalData.nif)
+      cy.personalLegalDataMember(this.data.personalLegalData, this.data.validAddress, this.is_physical)
+      cy.choosePaymentMethod()
+      cy.paymentData(this.data.personalLegalData.iban)
+      cy.checkReviewNewMemberStep(this.data.personalLegalData.nif)
+
+      cy.get('button').contains('Q9213530J').click()
+      cy.get('[data-cy=next]').click()
+      cy.get('[data-cy=next]').click()
+      cy.checkReviewNewMemberStep(this.data.personalLegalData.nif)
     })
   })
 })
--- cypress/support/newMemberCommands.js
+++ cypress/support/newMemberCommands.js
@@ -3,8 +3,7 @@ Cypress.Commands.add('identifyNewMember', (nif) => {
   cy.get('[data-cy=next]').click()
 })
 
-Cypress.Commands.add('personalDataMember', (personalData, validAddress, statusCode = 200) => {
-  console.log(personalData)
+Cypress.Commands.add('personalPhysicalDataMember', (personalData, validAddress, statusCode = 200) => {
   cy.get('[data-cy="new_member.name"]').type(personalData.name)
   cy.get('[data-cy="new_member.surname1"]').type(personalData.surname1)
   cy.get('[data-cy="new_member.surname2"]').type(personalData.surname2)
@@ -25,6 +24,28 @@ Cypress.Commands.add('personalDataMember', (personalData, validAddress, statusCo
   cy.get('[data-cy=next]').click()
 })
 
+Cypress.Commands.add('personalLegalDataMember', (personalData, validAddress, statusCode = 200) => {
+  cy.get('[data-cy="new_member.name"]').type(personalData.name)
+  cy.get('[data-cy="new_member.proxyname"]').type(personalData.proxyname)
+  cy.get('[data-cy="new_member.proxynif"]').type(personalData.proxynif)
+  cy.get('[data-cy="address-street"]').type(validAddress.input)
+  cy.contains(personalData.street).click()
+
+  cy.get('[data-cy="address.number"]').type('2')
+  cy.get('[data-cy="address.floor"]').type('1')
+  cy.get('[data-cy="address.door"]').type('3')
+  cy.get('[data-cy="address.stairs"]').type('B')
+  cy.get('[data-cy="address.bloc"]').type('Omega')
+  cy.get('[data-cy="email"]').type(personalData.email)
+  cy.get('[data-cy="repeat_email"]').type(personalData.email2)
+  cy.get('[data-cy="new_member.phone1"]').type(personalData.phone1)
+
+  cy.get('[data-cy="privacy_policy"]').click()
+  cy.get('[data-cy="legal_person"]').click()
+
+  cy.get('[data-cy=next]').click()
+})
+
 Cypress.Commands.add('choosePaymentMethod', ( creditCard = false ) => {
   const optionValue = creditCard ? 'credit-card':'iban'
   cy.get('[data-cy="method-payment-question"]').get(`[data-cy="${optionValue}"]`).click()
@@ -38,8 +59,8 @@ Cypress.Commands.add('paymentData', (iban) => {
   cy.get('[data-cy=next]').click()
 })
 
-Cypress.Commands.add('checkReviewNewMemberStep', (memberNumber) => {
-  cy.get('p').contains(memberNumber)
+Cypress.Commands.add('checkReviewNewMemberStep', (nif) => {
+  cy.get('button').contains(nif)
 })
 
 
