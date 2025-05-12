20e200faa14ba6cc6e97ec4bb2f74b742928754a
dukenuken09 <gilgassorovira@gmail.com>
2025-05-12 11:15:52 +0200

    ✨ Added new loader with new styles

---
+++ src/components/NewLoading.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import CircularProgress from '@mui/material/CircularProgress';
+import Typography from '@mui/material/Typography'
+import { useTranslation } from 'react-i18next'
+
+const NewLoading = (props) => {
+    const { t } = useTranslation()
+    const { description } = props
+    return (
+        <Box data-testid={'loading-component'} sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            gap: 3
+        }} >
+
+            <CircularProgress sx={{ color: 'secondary.main' }} />
+
+            <Typography variant="pagesubtitle">
+                {t(description)}
+            </Typography>
+
+        </Box>
+    )
+}
+
+export default NewLoading
