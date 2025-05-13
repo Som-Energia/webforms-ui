882293f9e13724a7a9fce73d92dfe5d1bfb8a2d3
Beatrigz <beatriz.plana@somenergia.coop>
2025-05-13 10:03:23 +0200

    🚧 🎨 style alert box

--- src/components/AlertBox.jsx
+++ src/components/AlertBox.jsx
@@ -2,14 +2,39 @@ import React from 'react'
 
 import Alert from '@mui/material/Alert'
 import AlertTitle from '@mui/material/AlertTitle'
-
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
+import { useTheme } from '@mui/material/styles'
+
+
 
 const AlertBox = ({ severity, title, description, children, variant, icon }) => {
+  const theme = useTheme ()
+
+  const primary2Color = theme.palette.primary2.main
+  const primary2Bg = '#ffebd2' //ligher background than the theme one
+  
+  const customSx =
+  severity === 'warning'
+    ? {
+        backgroundColor: primary2Bg,
+        border: 'none',
+        color: primary2Color,
+        '& .MuiAlertTitle-root': {
+          color: primary2Color,
+        },
+        '& .MuiTypography-root': {
+          color: primary2Color,
+        },
+        '& *': {
+          color: `${primary2Color} !important`,
+        },
+      }
+    : {}
+
   return (
     <Box mt={0.75} mb={1.5}>
-      <Alert severity={severity} icon={false}>
+      <Alert severity={severity} icon={false} sx={customSx}>
         <AlertTitle>{title}</AlertTitle>
         {description && (
           <Typography
