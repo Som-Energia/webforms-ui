b95e1dcc87610ba4a75a961ab25e331b30564908
Beatrigz <beatriz.plana@somenergia.coop>
2025-06-06 13:12:14 +0200

    ✨ add postal code field ✨ add not autocomplete option

--- src/components/AddressField.jsx
+++ src/components/AddressField.jsx
@@ -29,12 +29,9 @@ const AddressField = (props) => {
   const handleChange = useHandleChange(setFieldValue)
   const handleChangeInteger = useHandleChangeInteger(setFieldValue)
 
-  const [addressValue, setAddressValue] = useState(
-    values[addressFieldName]?.street
-  )
-  const [numberValue, setNumberValue] = useState(
-    values[addressFieldName]?.number
-  )
+  const [addressValue, setAddressValue] = useState(values[addressFieldName] || {})
+  const [numberValue, setNumberValue] = useState(values[addressFieldName]?.number || '')
+  const [postalCodeValue, setPostalCodeValue] = useState(values[addressFieldName]?.postal_code || '')
   const sessionTokenRef = useRef()
 
   useEffect(() => {
@@ -45,25 +42,31 @@ const AddressField = (props) => {
     }
   }, [addressValue, numberValue])
 
+  useEffect(() => {
+    setPostalCodeValue(values[addressFieldName]?.postal_code || '')
+  }, [values[addressFieldName]?.postal_code])
+
   const updateAddressValues = async () => {
     try {
       const place = await getPlaceDetails(addressValue.id, sessionTokenRef)
-      const postalCode = place.addressComponents.find((component) =>
+      const postalCodeComponent = place.addressComponents.find((component) =>
         component.types.includes('postal_code')
       )
-      const municipis = await getMunicipisByPostalCode(postalCode?.longText)
-      const street = place.addressComponents.find((component) =>
+      const municipis = await getMunicipisByPostalCode(postalCodeComponent?.longText)
+      const streetComponent = place.addressComponents.find((component) =>
         component.types.includes('route')
       )
 
       const updatedValues = { ...values }
       updatedValues[addressFieldName] = {
         ...values[addressFieldName],
+        id: addressValue.id,
+        text: addressValue.text,
         number: numberValue,
-        postal_code: postalCode?.longText || '',
-        street: street?.longText || '',
-        state: municipis && municipis[0] ? municipis[0][0]?.provincia : {},
-        city: municipis && municipis[0] ? municipis[0][0]?.municipi : {}
+        postal_code: postalCodeComponent?.longText || '',
+        street: streetComponent?.longText || '',
+        state: municipis?.[0]?.[0]?.provincia || {},
+        city: municipis?.[0]?.[0]?.municipi || {}
       }
       setValues(updatedValues)
     } catch (error) {
@@ -73,7 +76,7 @@ const AddressField = (props) => {
 
   const cleanAddress = () => {
     setFieldValue(addressFieldName, {
-      has_different_address: values[addressFieldName].has_different_address,
+      has_different_address: values[addressFieldName]?.has_different_address || false,
       street: '',
       number: '',
       postal_code: '',
@@ -82,29 +85,73 @@ const AddressField = (props) => {
     })
   }
 
-  const handleAddressChange = (value) => {
-    setAddressValue(value)
+  const handleAddressChange = async (value) => {
+    setAddressValue(value || {})
+
+    if (!value || !value.id) {
+      // Is a text is written, and no suggestion selected, the text is saved as street
+      setFieldValue(`${addressFieldName}.street`, value?.street || value?.text || '')
+      setFieldValue(`${addressFieldName}.postal_code`, '')
+      return
+    }
+
+    try {
+      // When a suggestion is selected, only street is written
+       const place = await getPlaceDetails(value.id, sessionTokenRef)
+
+      const streetComponent = place.addressComponents.find((component) =>
+        component.types.includes('route')
+      )
+      const postalCodeComponent = place.addressComponents.find((component) =>
+        component.types.includes('postal_code')
+      )
+
+      setFieldValue(`${addressFieldName}.street`, streetComponent?.longText || '')
+      setFieldValue(`${addressFieldName}.postal_code`, postalCodeComponent?.longText || '')
+    } catch (error) {
+      console.error('Error fetching place details:', error)
+      setFieldValue(`${addressFieldName}.street`, value.text || value.street || '')
+      setFieldValue(`${addressFieldName}.postal_code`, '')
+    }
+  }
+  const handleChangePostalCode = (event) => {
+    const value = event.target.value
+    setPostalCodeValue(value)
+    setFieldValue(`${addressFieldName}.postal_code`, value)
   }
 
   const handleChangeNumber = (event) => {
     let cleanedValue = event.target.value.replace(/[^0-9]/g, '')
     setNumberValue(cleanedValue)
+    setFieldValue(`${addressFieldName}.number`, cleanedValue)
   }
 
   return (
     <Grid container spacing={2}>
-      <Grid item sm={12} xs={12}>
+      <Grid item sm={8} xs={12}>
         <LocationInput
-          required
+          id={addressFieldName}
           textFieldLabel={t('GURB_ADDRESS_LABEL')}
           textFieldName={addressLabel}
-          id={`${addressFieldName}-street`}
-          name={`${addressFieldName}.street`}
-          value={addressValue}
+          value={values[addressFieldName]}
           onChange={handleAddressChange}
           sessionTokenRef={sessionTokenRef}
+          required={true}
         />
       </Grid>
+
+      <Grid item sm={4} xs={12}>
+        <InputField
+          name={`${addressFieldName}.postal_code`}
+          textFieldName={t('POSTAL_CODE')}
+          handleChange={handleChangePostalCode}
+          touched={touched[addressFieldName]?.postal_code}
+          value={postalCodeValue}
+          error={errors[addressFieldName]?.postal_code}
+          required={true}
+        />
+      </Grid>
+
       <Grid item sm={4} xs={12}>
         <InputField
           name={`${addressFieldName}.number`}
@@ -113,9 +160,10 @@ const AddressField = (props) => {
           touched={touched[addressFieldName]?.number}
           value={numberValue}
           error={errors[addressFieldName]?.number}
-          required={false}
+          required={true}
         />
       </Grid>
+
       <Grid item sm={2} xs={6}>
         <InputField
           name={`${addressFieldName}.floor`}
