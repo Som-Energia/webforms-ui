7ce21110e5acb6a2d49dc801295dfa35768e8a8a
marta197 <marta.fernandez@somenergia.coop>
2025-06-06 10:06:53 +0200

    ✅ test for newNormalizeContract: newMember

--- src/services/utilsMockData/forms/newContract/index.js
+++ src/services/utilsMockData/forms/newContract/index.js
@@ -179,10 +179,74 @@ const sponsored = {
   }
 }
 
+const newMember = {
+  entryValues: {
+    cups: 'ES0031405905577001DH0F',
+    has_member: 'member-off',
+    has_light: 'light-on',
+    previous_holder: 'previous-holder-yes',
+    voluntary_donation: true,
+    cadastral_reference_valid: true,
+    supply_point: {
+      cnae: 9820,
+      supply_point_accepted: true,
+      is_housing: true,
+      cnae_valid: true
+    },
+    supply_point_address: address.entryValues,
+    address: address.entryValues,
+    member: {
+      number: '',
+      nif: ''
+    },
+    new_member: client.physical.entryValues,
+    contract: {
+      tariff_mode: 'periods',
+      power_type: 'power-lower-15kw',
+      power: {
+        power1: '2',
+        power2: '2'
+      }
+    },
+    has_selfconsumption: 'selfconsumption-off',
+    self_consumption: {
+      cau_error: false,
+      installation_type: '',
+      technology: 'b11',
+      installation_power: ''
+    },
+    privacy_policy_accepted: true,
+    generic_conditions_accepted: true,
+    statutes_accepted: true,
+    comercial_info_accepted: false,
+    is_client: false
+  },
+  normalizedData: {
+    linked_member: 'new_member',
+    contract_info: contract_info_c1_20TD(),
+    contract_owner: {
+      ...client.physical.normalizedData,
+      address: address.normalizedData
+    },
+    new_member_info: {
+      ...client.physical.normalizedData,
+      address: address.normalizedData
+    },
+    donation: true,
+    iban: 'ES12 3456 7891 2345 6789 1234',
+    member_payment_type: 'remesa',
+    sepa_accepted: true,
+    general_contract_terms_accepted: true,
+    privacy_conditions: true,
+    statutes_accepted: true
+  }
+}
+
 const newContractCases = {
   // Member cases
   alreadyMember: alreadyMember,
-  sponsored: sponsored
+  sponsored: sponsored,
+  newMember: newMember
 }
 
 export default newContractCases
