7393fff558bb085666532b9ae845b9193a535641
dukenuken09 <gilgassorovira@gmail.com>
2025-07-18 11:23:15 +0200

    🔥🐢 Fix matomo tracker

--- src/trackers/matomo/MatomoTracker.js
+++ src/trackers/matomo/MatomoTracker.js
@@ -1,39 +1,40 @@
 class MatomoTracker {
-    constructor() {
-        this.initialize();
+  constructor() {
+    this.initialize();
+  }
+
+  initialize() {
+
+    if (document.getElementById('matomo_tracker') === null) {
+      var _paq = window._paq = window._paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function () {
+        var u = import.meta.env.VITE_MATOMO_URL;
+        _paq.push(['setTrackerUrl', u + 'matomo.php']);
+        _paq.push(['setSiteId', import.meta.env.VITE_MATOMO_SITE_ID]);
+        var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+        g.async = true; g.id = 'matomo_tracker'; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
+      })();
+    }
+  }
+
+  trackEvent({ category, action, name, value }) {
+    if (category && action) {
+      this.pushInstruction('trackEvent', category, action, name, value);
+    } else {
+      throw new Error(`Error: category and action are required.`);
+    }
+  }
+
+  pushInstruction(matomoFunctionName, category, action, name, value) {
+    if (typeof window !== 'undefined') {
+      window._paq.push([matomoFunctionName, category, action, name, value]);
     }
 
-    initialize() {
-        const matomoUrl = import.meta.env.VITE_MATOMO_URL
-        this._paq = window._paq = window._paq || []
-
-        const doc = document
-        const scriptElement = doc.createElement('script')
-        const scripts = doc.getElementsByTagName('script')[0]
-
-        scriptElement.type = 'text/javascript'
-        scriptElement.async = true
-        scriptElement.defer = true
-        scriptElement.src = matomoUrl
-
-        scripts.parentNode.insertBefore(scriptElement,scripts);
-    }
-
-    trackEvent({ category, action, name, value }) {
-        if (category && action) {
-          this.pushInstruction('trackEvent', category, action, name, value);
-        } else {
-          throw new Error(`Error: category and action are required.`);
-        }
-    }
-
-    pushInstruction(matomoFunctionName,  category, action, name, value) {
-        if (typeof window !== 'undefined') {
-          window._paq.push([matomoFunctionName, category, action, name, value]);
-        }
-    
-        return this;
-    }
+    return this;
+  }
 }
 
 export default MatomoTracker
