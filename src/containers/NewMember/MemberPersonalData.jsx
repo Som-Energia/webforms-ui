a08f94f721f545bab983528d9169e03f7e2e0804
Marite Guerrieri <marite.guerrieri@somenergia.coop>
2025-08-05 09:46:16 +0200

    🐛 state and city in newmember

--- src/containers/NewMember/MemberPersonalData.jsx
+++ src/containers/NewMember/MemberPersonalData.jsx
@@ -1,7 +1,6 @@
 import { useTranslation } from 'react-i18next'
 
 import Grid from '@mui/material/Grid'
-import Typography from '@mui/material/Typography'
 
 import LegalMemberPersonalData from './LegalMemberPersonalData'
 import PhysicalMemberPersonalData from './PhysicalMemberPersonalData'
