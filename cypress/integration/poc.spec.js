6c335876fc34ef743fe4e4d8ff67828a0dcfea12
javikalsan <1070397+javikalsan@users.noreply.github.com>
2025-08-29 23:29:03 +0200

    🔧 Fixture data to stdout

--- cypress/integration/poc.spec.js
+++ cypress/integration/poc.spec.js
@@ -2,6 +2,7 @@ describe('Fixture Debug', () => {
   it('can load cancellation.json', () => {
     cy.fixture('cancellation.json').then((data) => {
       cy.log(JSON.stringify(data))
+      console.log(data)
     })
   })
 })
