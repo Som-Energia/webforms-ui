276c74035a28a3d85bf5bbc27114b3f9083b7fb3
javikalsan <1070397+javikalsan@users.noreply.github.com>
2025-09-08 17:19:55 +0200

    🚧 tune env vars configuration

--- cypress/integration/poc.spec.js
+++
@@ -1,8 +0,0 @@
-describe('Fixture Debug', () => {
-  it('can load cancellation.json', () => {
-    cy.fixture('cancellation.json').then((data) => {
-      cy.log(JSON.stringify(data))
-      console.log(data)
-    })
-  })
-})
