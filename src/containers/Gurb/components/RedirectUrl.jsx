4c28b66f92710c9fc776532eca4e4851bf3ef524
javikalsan <1070397+javikalsan@users.noreply.github.com>
2025-09-10 16:59:11 +0200

    🚧 Use RedirectUrl component

--- src/containers/Gurb/components/RedirectUrl.jsx
+++ src/containers/Gurb/components/RedirectUrl.jsx
@@ -1,60 +1,66 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 
-
 const RedirectUrl = ({
-    title = 'somenergia redirect url component',
-    description = 'a naive url redirect component',
-    buttonText = 'Ves-hi!',
-    url = 'https://somenergia.coop'
+  title = 'somenergia redirect url component',
+  description = 'a naive url redirect component',
+  buttonText = 'Ves-hi!',
+  url = 'https://somenergia.coop',
 }) => {
-    const handleRedirect = () => {
-        if (url) {
-            window.location.href = url
-        }
-    }
-
-    return (
-      <Grid container spacing={2} direction="column" alignItems="center">
-
-        <Grid item xs={12}>
-          <Typography variant="h5" gutterBottom>
-              {title}
-          </Typography>
-        </Grid>
-
-        <Grid item xs={12}>
-          <Typography
-            sx={{
-              mt: 2,
-              fontWeight: '400',
-              fontSize: '1rem',
-              lineHeight: '1.75',
-              textAlign: 'center',
-              color: 'secondary.extraDark'
-            }}
-            variant="body1"
-            dangerouslySetInnerHTML={{
-                __html: description
-            }}
-          />
-        </Grid>
-
-        <Grid item xs={12}>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleRedirect}
-            sx={{ borderRadius: '2rem', paddingX: '2rem' }}
-          >
-            {buttonText}
-          </Button>
-        </Grid>
-
+  return (
+    <Grid container spacing={2} direction="column" alignItems="center">
+      <Grid item>
+        <Typography variant="h5" sx={{ textAlign: 'center' }}>
+          {title}
+        </Typography>
       </Grid>
-    )
+
+      <Grid item>
+        <Typography
+          component="div"
+          variant="body1"
+          sx={{
+            mt: 2,
+            fontWeight: 400,
+            fontSize: '1rem',
+            lineHeight: 1.75,
+            textAlign: 'center',
+            color: 'secondary.extraDark',
+          }}
+          {...(description
+            ? { dangerouslySetInnerHTML: { __html: description } }
+            : {})}
+        />
+      </Grid>
+
+      <Grid item>
+        <Button
+          component="a"
+          href={url}
+          variant="contained"
+          color="primary"
+          sx={{
+            borderRadius: '2rem',
+            px: '2rem',
+            textTransform: 'none',
+            color: 'white',
+          }}
+        >
+          {buttonText}
+        </Button>
+      </Grid>
+    </Grid>
+  )
+}
+
+RedirectUrl.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  buttonText: PropTypes.string,
+  url: PropTypes.string,
 }
 
 export default RedirectUrl
--- src/containers/Gurb/pages/Gurb/GurbRequirementsResult.jsx
+++ src/containers/Gurb/pages/Gurb/GurbRequirementsResult.jsx
@@ -1,14 +1,27 @@
 import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
 import CheckCircleIcon from '@mui/icons-material/CheckCircle'
 import { useTranslation } from 'react-i18next'
+import PropTypes from 'prop-types'
 
 import RedirectUrl from '../../../../containers/Gurb/components/RedirectUrl'
 
-const GurbRequirementsResult = () => {
+const GurbRequirementsResult = ({ values }) => {
   const { t } = useTranslation()
-  const title = 'rrrrrr'
-  const description = 'rrrrrr'
+  const { new_member, redirectUrl } = values
+
+  const resultTitle = new_member
+    ? t('GURB_REQUIREMENTS_RESULT_TITLE_NEW_MEMBER')
+    : t('GURB_REQUIREMENTS_RESULT_TITLE_EXISTING_MEMBER')
+
+  const resultDescription = new_member
+    ? t('GURB_REQUIREMENTS_RESULT_DESCRIPTION_NEW_MEMBER')
+    : t('GURB_REQUIREMENTS_RESULT_DESCRIPTION_EXISTING_MEMBER')
+
+  const resultRedirectUrl = redirectUrl ?? 'http://localhost:3000/ca/gurb/2/join/'
+
+  const resultButtonText = new_member
+    ? t('GURB_REQUIREMENTS_RESULT_BUTTON_TEXT_NEW_MEMBER')
+    : t('GURB_REQUIREMENTS_RESULT_BUTTON_TEXT_EXISTING_MEMBER')
 
   return (
     <Box
@@ -16,9 +29,9 @@ const GurbRequirementsResult = () => {
         pt: 4,
         display: 'flex',
         flexDirection: 'column',
-        alignItems: 'center'
-      }}>
-
+        alignItems: 'center',
+      }}
+    >
       <CheckCircleIcon
         data-cy="success-icon"
         fontSize="large"
@@ -26,13 +39,20 @@ const GurbRequirementsResult = () => {
       />
 
       <RedirectUrl
-        title={title}
-        description={description}
-        url="https://somenergia.coop"
+        title={resultTitle}
+        description={resultDescription}
+        url={resultRedirectUrl}
+        buttonText={resultButtonText}
       />
     </Box>
   )
+}
 
+GurbRequirementsResult.propTypes = {
+  values: PropTypes.shape({
+    new_member: PropTypes.bool.isRequired,
+    redirectUrl: PropTypes.string,
+  }).isRequired,
 }
 
 export default GurbRequirementsResult
