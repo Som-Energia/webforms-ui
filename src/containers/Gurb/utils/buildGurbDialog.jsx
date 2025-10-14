a67c57efc1c307f9ca669838cd92ce2d12167712
javikalsan <1070397+javikalsan@users.noreply.github.com>
2025-10-14 16:47:45 +0200

    💄 Prettier format

--- src/containers/Gurb/utils/buildGurbDialog.jsx
+++ src/containers/Gurb/utils/buildGurbDialog.jsx
@@ -2,34 +2,39 @@ import React from 'react'
 import Typography from '@mui/material/Typography'
 import SimpleGurbDialog from '../../../components/SimpleGurbDialog'
 
-export const buildGurbDialog = ({ severity, setContent, titleKey, text1Key, text2Key }) => (
-
-    <SimpleGurbDialog
-      severity={severity}
-      title={<Typography dangerouslySetInnerHTML={{ __html: titleKey }} />}
-      text1={
-        text1Key ? (
-          <Typography
-            sx={{ fontSize: 14 }}
-            dangerouslySetInnerHTML={{ __html: text1Key }}
-          />
-        ) : null
-      }
-      text2={
-        text2Key ? (
-          <Typography
-            sx={{
-              fontSize: 14,
-              a: {
-                color: 'black',
-                fontWeight: severity === 'warning' ? 'bold' : 'normal',
-                textDecoration: severity === 'warning' ? 'underline' : 'none'
-              },
-            }}
-            dangerouslySetInnerHTML={{ __html: text2Key }}
-          />
-        ) : null
-      }
-      closeFunction={async () => setContent(undefined)}
-    />
-  )
+export const buildGurbDialog = ({
+  severity,
+  setContent,
+  titleKey,
+  text1Key,
+  text2Key
+}) => (
+  <SimpleGurbDialog
+    severity={severity}
+    title={<Typography dangerouslySetInnerHTML={{ __html: titleKey }} />}
+    text1={
+      text1Key ? (
+        <Typography
+          sx={{ fontSize: 14 }}
+          dangerouslySetInnerHTML={{ __html: text1Key }}
+        />
+      ) : null
+    }
+    text2={
+      text2Key ? (
+        <Typography
+          sx={{
+            fontSize: 14,
+            a: {
+              color: 'black',
+              fontWeight: severity === 'warning' ? 'bold' : 'normal',
+              textDecoration: severity === 'warning' ? 'underline' : 'none'
+            }
+          }}
+          dangerouslySetInnerHTML={{ __html: text2Key }}
+        />
+      ) : null
+    }
+    closeFunction={async () => setContent(undefined)}
+  />
+)
