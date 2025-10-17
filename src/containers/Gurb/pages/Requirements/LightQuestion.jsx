99040a3f5dcd89d9f01bec0cd8b9ff091ed72247
javikalsan <1070397+javikalsan@users.noreply.github.com>
2025-10-17 13:13:54 +0200

    💄Prettierize

--- src/containers/Gurb/pages/Requirements/LightQuestion.jsx
+++ src/containers/Gurb/pages/Requirements/LightQuestion.jsx
@@ -15,7 +15,6 @@ import Grid from '@mui/material/Grid'
 import PopUpContext from '../../../../context/PopUpContext'
 import { buildGurbDialog } from '../../../../containers/Gurb/utils/buildGurbDialog'
 
-
 const LightQuestion = (props) => {
   const { values, setFieldValue } = props
   const { t } = useTranslation()
@@ -32,9 +31,9 @@ const LightQuestion = (props) => {
           severity: 'warning',
           setContent: setContent,
           text1Key: t('GURB_LIGHT_QUESTION_ERROR_MAIN_TEXT'),
-          text2Key: t('GURB_LIGHT_QUESTION_ERROR_SECONDARY_TEXT'),
-        }
-      ))
+          text2Key: t('GURB_LIGHT_QUESTION_ERROR_SECONDARY_TEXT')
+        })
+      )
     } else {
       setError(false)
     }
@@ -44,13 +43,12 @@ const LightQuestion = (props) => {
     {
       id: 'light-on',
       icon: <LightbulbOutlinedIcon sx={iconRequirements} />,
-      textHeader: t('GURB_HAS_LIGHT_HEADER'),
+      textHeader: t('GURB_HAS_LIGHT_HEADER')
     },
     {
       id: 'light-off',
       icon: <LightbulbOutlinedIcon sx={iconOffRequirements} />,
-      textHeader: t('GURB_HAS_NO_LIGHT_HEADER'),
-
+      textHeader: t('GURB_HAS_NO_LIGHT_HEADER')
     }
   ]
 
