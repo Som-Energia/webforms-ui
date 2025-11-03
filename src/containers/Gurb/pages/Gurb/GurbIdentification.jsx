0fcf36c3da24a6f0810b8702acdbba6bfb9d9bc1
javikalsan <1070397+javikalsan@users.noreply.github.com>
2025-11-03 16:49:15 +0100

    🚧 Walking the perfect pixel way 🚶

--- src/containers/Gurb/pages/Gurb/GurbIdentification.jsx
+++ src/containers/Gurb/pages/Gurb/GurbIdentification.jsx
@@ -16,7 +16,7 @@ const GurbIdentification = (props) => {
 
       <Grid item xs={12}>
         <Typography variant="headline4.regular">
-          {t('LINK_MEMBER_TITLE')}
+          {t('GURB_MEMBER_TITLE')}
         </Typography>
       </Grid>
 
--- src/containers/Gurb/pages/Gurb/GurbParticipation.jsx
+++ src/containers/Gurb/pages/Gurb/GurbParticipation.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState, useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
 import Typography from '@mui/material/Typography'
-import { textHeader4, textField, textHeader5, textHeader2 } from '../../gurbTheme'
+import {
+  textField,
+  textHeader5,
+} from '../../gurbTheme'
 import Box from '@mui/material/Box'
 import Select from '../../components/Select'
 import Alert from '@mui/material/Alert'
 import { getPowers } from '../../../../services/api'
-
-const TWENTY_TD_TARIFF = "2.0 TD"
 
 const GurbParticipation = (props) => {
   const { values, setFieldValue, gurbCode } = props
@@ -27,47 +28,89 @@ const GurbParticipation = (props) => {
         })
         setFieldValue('gurb.join_cost', response.data.initial_quota)
       })
-      .catch(error => console.log(error))
+      .catch((error) => console.log(error))
   }, [gurbCode, values.tariff_name])
 
   useEffect(() => {
     getAvailablePowers()
   }, [getAvailablePowers])
 
+  const onChangePower = useCallback(
+    async (value) => {
+      await setFieldValue('gurb.power', value)
+      await setFieldValue('gurb.daily_cost', Number(gurbDetails.quota * value))
+    },
+    [setFieldValue, gurbDetails]
+  )
 
-  const onChangePower = useCallback(async (value) => {
-    await setFieldValue('gurb.power', value)
-    await setFieldValue('gurb.daily_cost', Number(gurbDetails.quota * value))
-  }, [setFieldValue, gurbDetails])
-
+  const TWENTY_TD_TARIFF = '2.0TD'
+  const informationTextKey =
+    values.tariff_name === TWENTY_TD_TARIFF
+      ? 'GURB_PARTICIPATION_TEXT_20'
+      : 'GURB_PARTICIPATION_TEXT_30'
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-      <Typography sx={{ ...textHeader2, mb: 5 }}>{t('GURB_PARTICIPATION')}</Typography>
-      <Typography sx={textHeader4}>{t('GURB_PARTICIPATION_KW_INPUT_TEXT')}</Typography>
-      <Typography sx={textHeader5}>{t('GURB_PARTICIPATION_KW_INPUT_TEXT_SECONDARY')}</Typography>
+
+      <Typography variant="headline4.regular">
+        {t('GURB_PARTICIPATION_KW_INPUT_TEXT')}
+      </Typography>
+
+      <Typography sx={textHeader5}>
+        {t('GURB_PARTICIPATION_KW_INPUT_TEXT_SECONDARY')}
+      </Typography>
+
       <Select
         options={gurbDetails.available_betas ?? []}
         value={values.gurb.power}
         handleChange={(value) => onChangePower(value)}
         style={textField}
-        helperText={
+      />
+
+      <Typography
+        sx={{
+          '& a, & a:link, & a:visited, & a:active': {
+            fontFamily: 'Inter',
+            color: '#919191 !important',
+            fontSize: '14px'
+          }
+        }}>
+        {
           <span
             dangerouslySetInnerHTML={{
               __html: t('GURB_HELP_ANNUAL_CONSUMPTION', {
                 url: t('GURB_HELP_ANNUAL_CONSUMPTION_URL')
               })
-            }}
-          />
+            }}></span>
         }
-      />
-      {values.tariff_name === TWENTY_TD_TARIFF ?
-        <Alert severity='warning'>
-          <Typography color="inherit" align='justify' dangerouslySetInnerHTML={{
-            __html: t('GURB_PARTICIPATION_TEXT_1')
-          }} />
-        </Alert>
-        : null}
+      </Typography>
+
+      <Alert
+        severity="info"
+        sx={{
+          mt: 4,
+          mb: 4,
+          color: '#ed6c02',
+          backgroundColor: 'rgb(255, 244, 229)',
+          '& .MuiAlert-icon': {
+            color: '#ed6c02',
+            mt: '0.3em'
+          }
+        }}>
+        <Typography
+          sx={{
+            color: '#ed6c02',
+            '& a, & a:link, & a:visited, & a:active': {
+              color: '#ed6c02 !important',
+              textDecoration: 'underline'
+            }
+          }}
+          dangerouslySetInnerHTML={{
+            __html: t(informationTextKey)
+          }}
+        />
+      </Alert>
+
     </Box>
   )
 }
