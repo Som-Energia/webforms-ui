f845989590b5f2bffbf0eec2fdc1a66e6f18fd20
Mario <mario.martinez@somenergia.coop>
2026-01-12 10:18:53 +0100

    ✅  add AutocompleteFloorInput.test.jsx

--- src/components/AutocompleteFloorInput/AutocompleteFloorInput.jsx
+++ src/components/AutocompleteFloorInput/AutocompleteFloorInput.jsx
@@ -46,33 +46,17 @@ export default function SomAutocompleteFloorInput({
 
   return (
     <>
-      <Grid container spacing={1}>
-        {title && (
-          <Grid item xs={helper ? 6 : 12} sx={{ mb: '6px' }}>
-            <InputTitle text={title}></InputTitle>
-          </Grid>
-        )}
-        {title && helper && (
-          <Grid item xs={6}>
-            <Typography variant="body.sm.regular" color="secondary.extraDark">
-              {helper}
-            </Typography>
-          </Grid>
-        )}
-        <Grid item xs={12}>
-          <Autocomplete
-            className={'somAutocompleteInput'}
-            freeSolo
-            disableClearable
-            clearOnBlur
-            onBlur={handleAutocompleteBlur}
-            value={optionValue}
-            options={autocompleteOptions}
-            renderInput={(params) => <TextField {...params} name={fieldName} />}
-          />
-          <input type="hidden" value={value} name={fieldName}></input>
-        </Grid>
-      </Grid>
+      <Autocomplete
+        className={'somAutocompleteInput'}
+        freeSolo
+        disableClearable
+        clearOnBlur
+        onBlur={handleAutocompleteBlur}
+        value={optionValue}
+        options={autocompleteOptions}
+        renderInput={(params) => <TextField {...params} />}
+      />
+      <input type="hidden" value={value} name={fieldName}></input>
     </>
   )
 }
