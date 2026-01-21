85ac46471206d86cc1d63a059924667a37010657
Benji <benjami.ramos@somenergia.coop>
2026-01-21 14:06:23 +0100

    ♻️  Moved chooser styles to dedicated file
    Co-authored-by: Joana Figueira <joana.figueira@somenergia.coop>

---
+++ src/components/Chooser/chooserStyles.js
@@ -0,0 +1,15 @@
+
+export const chooserStyles = {
+    paddingTop: '1.5rem',
+    paddingBottom: '2rem',
+    paddingLeft: '1.625rem',
+    paddingRight: '1.625rem',
+    borderRadius: '8px',
+    border: '1px solid #D9D9D9',
+    cursor: 'pointer'
+}
+
+export const chooserSelectedStyles = {
+    ...chooserStyles,
+    backgroundColor: 'secondary.extraLight'
+}
