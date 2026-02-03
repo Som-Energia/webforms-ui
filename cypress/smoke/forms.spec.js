1694b5679f78ac21d3c3f5d27996298effe2474c
joana <joana.figueira@somenergia.coop>
2026-02-03 10:02:28 +0100

    ✅ Add first smoke tests to somenergia webpage

---
+++ cypress/smoke/forms.spec.js
@@ -0,0 +1,30 @@
+describe('Sanity check for contract form', () => {
+    it('Button to go to contract main page should work', () => {
+        cy.request('/ca/contracta-la-llum-domestic/').its('status').should('eq', 200)
+    })
+    it('Button to go to contract 20TD form should work', () => {
+        cy.request('/ca/formulari-contractacio-periodes/?form_type=domestic').its('status').should('eq', 200)
+    })
+    it('Button to go to contract indexed form should work', () => {
+        cy.request('/ca/formulari-contractacio-indexada/?form_type=domestic').its('status').should('eq', 200)
+    })
+    it('Button to go to enterprise contract 20TD form should work', () => {
+        cy.request('/ca/formulari-contractacio-periodes/?form_type=enterprise').its('status').should('eq', 200)
+    })
+
+})
+
+describe('Sanity check for new member form', () => {
+    it('Button to go to new member main page should work', () => {
+        cy.request('/ca/cooperativa/associar-se').its('status').should('eq', 200)
+    })
+    it('Button to go to new member form should work', () => {
+        cy.request('/ca/cooperativa/formulari-associar-se').its('status').should('eq', 200)
+    })
+})
+
+describe('Sanity check for gurb form', () => {
+    it('Button to go to gurb main page should work', () => {
+        cy.request('ca/serveis/generacio-urbana/').its('status').should('eq', 200)
+    })
+})
