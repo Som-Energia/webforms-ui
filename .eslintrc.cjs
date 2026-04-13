da7835b8356fc1922a1e7cf188552a890eb06655
Mario <mario.martinez@somenergia.coop>
2026-04-13 12:05:28 +0200

    🏗️ 🎨 configurations prettier and eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,11 +11,45 @@ module.exports = {
   ignorePatterns: ['dist', 'forms', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: 'detect' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'simple-import-sort', 'unused-imports'],
   rules: {
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
+    ],
     'react/prop-types': 'off',
-    'eqeqeq': ['error', 'always']
-  },
+    eqeqeq: ['error', 'always'],
+    'arrow-parens': ['error', 'always'],
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Side effects
+          ['^\\u0000'],
+          // React
+          ['^react', '^react-dom', '^react-router', '^react-i18next'],
+          // @mui
+          ['^@mui/'],
+          // @somenergia
+          ['^@somenergia/'],
+          // Externos (resto)
+          ['^@?\\w'],
+          // Internos
+          ['^\\.\\./', '^\\./', '^\\.']
+        ]
+      }
+    ],
+    'simple-import-sort/exports': 'error',
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_'
+      }
+    ]
+  }
 }
-
