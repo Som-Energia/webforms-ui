610800045f954dcabf279bd925aaa0ee30bac9ba
Marite Guerrieri <marite.guerrieri@somenergia.coop>
2026-07-01 12:09:15 +0200

    🐛 CORS error

--- src/components/PDFLoader/PDFLoader.jsx
+++ src/components/PDFLoader/PDFLoader.jsx
@@ -10,33 +10,24 @@ const PDFLoader = (props) => {
     documentName,
     language = `${i18n.language}_ES`,
   } = props
-  const [text, setText] = React.useState()
+
+  const [url, setUrl] = React.useState(undefined)
 
   React.useEffect(() => {
-    const url = `${import.meta.env.VITE_BACKOFFICE_URL}/storage/app/media/DOCS/legal/${language.slice(0,2)}/${folder}/${documentName}.pdf`
-    setText(undefined)
-    axios({
-      method: 'GET',
-      url: url,
-      responseType: "blob"
-    }).then((response) => {
-      const pdfUrl = URL.createObjectURL(response?.data);
-      setText(pdfUrl)
-    }).catch(() => {
-      console.log(`Error retrieving text ${url}`)
-    })
+    const pdfUrl = `${import.meta.env.VITE_BACKOFFICE_URL}/storage/app/media/DOCS/legal/${language.slice(0,2)}/${folder}/${documentName}.pdf`
+    setUrl(pdfUrl)
   }, [documentName, language])
 
-  return text === undefined
+  return url === undefined
     ? <Loading />
     : <object
-      data={text}
-      title="PDF"
-      type="application/pdf"
-      width="100%"
-      height="100%"
-      style={{ border: "none" }}
-    />
+        data={url}
+        title="PDF"
+        type="application/pdf"
+        width="100%"
+        height="100%"
+        style={{ border: "none" }}
+      />
 
 }
 
