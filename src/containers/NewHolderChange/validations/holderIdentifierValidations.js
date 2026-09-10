a9cb52f839586de7b75be8be93515ac8e48a735a
Marite Guerrieri <marite.guerrieri@somenergia.coop>
2026-09-10 14:38:18 +0200

    ✨ new holder change form

--- src/containers/NewHolderChange/validations/holderIdentifierValidations.js
+++ src/containers/NewHolderChange/validations/holderIdentifierValidations.js
@@ -1,14 +1,19 @@
 import * as Yup from "yup"
 
 const holderIdentifierValidations = Yup.object().shape({
-  new_member: Yup.object().shape({
-    nif: Yup.string()
-      .required("ERROR_REQUIRED_FIELD")
-      .matches(/(^[A-GI-Z0-9])/, "CIF_COMMUNITY_OWNERS")
-      .matches(
-        /^([0-9]{8}[A-Z])|([A-Z][0-9]{7}[A-Z])|([A-Z][0-9]{8})$/,
-        "INVALID_NIF",
-      ),
+  new_member: Yup.object().when("has_member", {
+    is: (value) => ["member-off", "no-member"].includes(value),
+    then: (schema) =>
+      schema.shape({
+        nif: Yup.string()
+          .required("ERROR_REQUIRED_FIELD")
+          .matches(/^[A-GI-Z0-9]/, "CIF_COMMUNITY_OWNERS")
+          .matches(
+            /^([0-9]{8}[A-Z])|([A-Z][0-9]{7}[A-Z])|([A-Z][0-9]{8})$/,
+            "INVALID_NIF",
+          ),
+      }),
+    otherwise: (schema) => schema.notRequired(),
   }),
   member: Yup.object().when("has_member", {
     is: (value) => ["member-link", "member-on"].includes(value),
--- src/containers/NewHolderChange/validations/newHolderChangeEspecialCasesValidations.js
+++ src/containers/NewHolderChange/validations/newHolderChangeEspecialCasesValidations.js
@@ -1,5 +1,9 @@
 import * as Yup from "yup"
 
+const requiredAttachments = Yup.array()
+  .min(1, "ERROR_REQUIRED_FIELD")
+  .required("ERROR_REQUIRED_FIELD")
+
 const newHolderChangeEspecialCasesValidations = Yup.object().shape({
   especial_cases: Yup.string()
     .required("REQUIRED_FIELD")
@@ -9,6 +13,28 @@ const newHolderChangeEspecialCasesValidations = Yup.object().shape({
       "reason_merge",
       "reason_electrodep",
     ]),
+  supply_point: Yup.object().when("especial_cases", (value, schema) => {
+    switch (value) {
+      case "reason_death":
+        return schema.shape({
+          attachments_reason_death: requiredAttachments,
+        })
+
+      case "reason_merge":
+        return schema.shape({
+          attachments_reason_merge: requiredAttachments,
+        })
+
+      case "reason_electrodep":
+        return schema.shape({
+          attachments_reason_electrodep: requiredAttachments,
+          attachments_reason_electrodep_census: requiredAttachments,
+        })
+
+      default:
+        return schema.notRequired()
+    }
+  }),
 })
 
 export default newHolderChangeEspecialCasesValidations
